2b757c0fe2ca7f78868591624f5e6e4e4f8d5184
stephen <milessa42@gmail.com>
2014-06-25 15:07:21 -0400

    Area links on People index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def title( page_title )
     content_for(:title) { page_title }
   end
+
+  def person_area_links(person)
+    links = Array.new
+    for area in person.areas do
+      #TODO Add controller path for areas
+      links << link_to(area.name, '#')
+    end
+    links.join(', ').html_safe
+  end
 end
