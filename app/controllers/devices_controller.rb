604a3860b785357e40653a56996e664405f16c33
stephen <milessa42@gmail.com>
2014-07-03 14:52:57 -0400

    Beginning of device show view

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -5,6 +5,8 @@ class DevicesController < ApplicationController
   end
 
   def show
+    @device = Device.find params[:id]
+    @log_entries = LogEntry.where("(trackable_type = 'Device' AND trackable_id = #{@device.id}) OR (trackable_type = 'DeviceDeployment' AND referenceable_id = #{@device.id})").order('created_at DESC')
   end
 
   def new
