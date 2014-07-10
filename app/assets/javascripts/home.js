cb9499fd2551c02add48e6b39102e7558e6ffa24
stephen <milessa42@gmail.com>
2014-07-10 16:44:02 -0400

    Sales Chart faked and Masonry removed

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,67 +1,58 @@
-//= require masonry.min
 //= require google_jsapi
 //= require chartkick
 
 
 $(function () {
 	$(document).foundation();
-	var $widgets = $('.masonry');
-
-	$widgets.masonry({
-		columnWidth: '.widget-sizer',
-		isAnimated: true,
-		itemSelector: '.widget'
-	});
 
 	$.get("/widgets/sales", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/hours", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/tickets", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/social", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/alerts", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/image_gallery", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/inventory", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/staffing", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/gaming", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/commissions", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/training", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	$.get("/widgets/gift_cards", function( data ) {
-		$( ".masonry").append( data );
+		$( ".widgets").append( data );
 	});
 
 	//TODO: insure placement order of widgets
 
-	//new Chartkick.LineChart("saleschart", {"2013-02-10 00:00:00 -0800": 11, "2013-02-11 00:00:00 -0800": 6});
 });
