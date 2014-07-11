1451783ba058b42c82cb0237775317549dd09969
Anthony Atkinson <aatkinson@retaildoneright.com>
2014-07-11 12:19:24 -0400

    Commissions and training widgets along with styling for all.

---
+++ app/models/connect_order_payout.rb
@@ -0,0 +1,21 @@
+# Openbravo commission payments
+class ConnectOrderPayout < RealConnectModel
+  # Openbravo table name
+  self.table_name = 'rc_order_payout'
+  # Openbravo table primary key column
+  self.primary_key = 'rc_order_payout_id'
+
+  # Relationship for the Openbravo asset records
+  belongs_to :connect_user,
+           primary_key: 'ad_user_id',
+           foreign_key: 'ad_user_id'
+  belongs_to :connect_order,
+             primary_key: 'c_order_id',
+             foreign_key: 'c_order_id'
+
+  def self.this_week
+    beginning_date_time = Date.today.beginning_of_week.to_datetime
+    end_date_time = Date.today.end_of_week.to_datetime
+    self.joins(:connect_order).where('dateordered >= ?', beginning_date_time).where('dateordered < ?', end_date_time)
+  end
+end
