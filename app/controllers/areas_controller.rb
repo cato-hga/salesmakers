4c5b27ceddc57577c11010824977d7394bbaab11
stephen <milessa42@gmail.com>
2014-07-16 12:06:14 -0400

    Area widgets created, currently columns are not nesting inside rows

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,7 +1,12 @@
 class AreasController < ApplicationController
   def index
     @project = Project.find params[:project_id]
-    @areas = Area.where(project: @project).arrange(order: :name)
+    all_areas = Area.where(project: @project).arrange(order: :name)
+    if all_areas and not all_areas.empty?
+      @areas = all_areas.first.first.siblings.where project: @project
+    else
+      []
+    end
   end
 
   def show
