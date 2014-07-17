74bce27e582e3069576051216035436d27d9396b
stephen <milessa42@gmail.com>
2014-07-17 10:53:48 -0400

    Areas now expandable and collapsible

---
+++ app/assets/javascripts/areas.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.nested_areas a').bind('click', function(){
+		iconElement = $(this).children('i');
+		if (iconElement.hasClass('fi-plus')) {
+			$(this).parent().next('ul').find('li').show();
+			$(this).children('i').removeClass('fi-plus').addClass('fi-minus');
+		} else {
+			$(this).parent().next('ul').find('li').hide();
+			$(this).children('i').removeClass('fi-minus').addClass('fi-plus');
+		}
+	});
+});
