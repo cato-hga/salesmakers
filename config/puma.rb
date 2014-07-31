cf7335fc988798104c0fa96c50d8c8d832a6da39
stephen <milessa42@gmail.com>
2014-07-31 09:40:01 -0400

    Commit for deployment testing

---
+++ config/puma.rb
@@ -0,0 +1,9 @@
+threads 1, 16
+workers 4
+
+on_worker_boot do
+  require "active_record"
+  cwd = File.dirname(__FILE__)+"/.."
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"] || YAML.load_file("#{cwd}/config/database.yml")[ENV["RAILS_ENV"]])
+end
