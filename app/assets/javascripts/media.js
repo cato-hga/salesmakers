7026e4bfa7d9ff2959cffe0f665d2c47ac64cb39
stephen <milessa42@gmail.com>
2014-08-15 13:24:33 -0400

    Media page and controller started. Swiper on Media#index is currently not rotating and looping correctly.
    Issue with Swiper CSS for home#index being overwritten by media#index is resolved.

--- app/assets/javascripts/media.js
+++ app/assets/javascripts/media.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+//= require swiper
+
+$(function(){
+	var width = $(window).width();
+	var slidesPer = 1;
+	if (width > 760) slidesPer = 2;
+	if (width > 1270) slidesPer = 3;
+	var mySwiper = $('.swiper-container').swiper({
+		mode: 'horizontal',
+		loop: true,
+		centeredSlides: true,
+		speed: 600,
+		autoplay: 4000,
+		slidesPerView: 'auto',
+		calculateHeight: true,
+		loopedSlides: 10
+	});
+});
