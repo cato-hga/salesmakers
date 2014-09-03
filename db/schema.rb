2a5093305a71212a9da8367f208867e28ac51c61
stephen <milessa42@gmail.com>
2014-09-03 16:37:10 -0400

    post migration commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902135934) do
+ActiveRecord::Schema.define(version: 20140903203640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -146,6 +146,7 @@ ActiveRecord::Schema.define(version: 20140902135934) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_me_user_id",  null: false
+    t.string   "message_num",       null: false
   end
 
   create_table "group_me_users", force: true do |t|
