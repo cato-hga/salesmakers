a512b0e6447a9bc347dc9fe82731430c7657def6
stephen <milessa42@gmail.com>
2014-09-16 15:43:54 -0400

    Phone validator error messages make more sense now

--- app/models/validators/phone_number_validator.rb
+++ app/models/validators/phone_number_validator.rb
@@ -1,20 +1,20 @@
 class PhoneNumberValidator < ActiveModel::Validator
   def validate(record)
     unless record.mobile_phone.present? or record.office_phone.present? or record.home_phone.present?
-      record.errors[:mobile_phone] << 'At least one phone number is required' #TODO: Format this error
+      record.errors[:mobile_phone] << 'is required if there is no Home or Office phone' #TODO: Format this error
       return
     end
     record.mobile_phone = record.mobile_phone.strip.gsub /[^0-9]/, '' if record.mobile_phone.present?
     record.home_phone = record.home_phone.strip.gsub /[^0-9]/, '' if record.home_phone.present?
     record.office_phone = record.office_phone.strip.gsub /[^0-9]/, '' if record.office_phone.present?
     if record.mobile_phone.present? and record.mobile_phone.length != 10
-      record.errors[:mobile_phone] << 'Mobile phone number must be 10 digits long and include your area code'
+      record.errors[:mobile_phone] << 'number must be 10 digits long and include your area code'
     end
     if record.home_phone.present? and record.home_phone.length != 10
-      record.errors[:home_phone] << 'Home phone number must be 10 digits long and include your area code'
+      record.errors[:home_phone] << 'number must be 10 digits long and include your area code'
     end
     if record.office_phone.present? and record.office_phone.length != 10
-      record.errors[:office_phone] << 'Office phone number must be 10 digits long and include your area code'
+      record.errors[:office_phone] << 'number must be 10 digits long and include your area code'
     end
   end
 end
