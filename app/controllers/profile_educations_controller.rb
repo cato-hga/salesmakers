ba350b2a2001be527cb6038f5e3680b4a15de844
stephen <milessa42@gmail.com>
2014-09-17 15:01:18 -0400

    Educations now showing, and can be added

--- app/controllers/profile_educations_controller.rb
+++ app/controllers/profile_educations_controller.rb
@@ -1,16 +1,46 @@
 class ProfileEducationsController < ApplicationController
   def new
+    person = @current_person
+    @profile_education = ProfileEducation.new profile: person.profile
   end
 
   def edit
-  end
-
-  def update
+    @profile_education = ProfileEducation.find params[:id]
   end
 
   def create
+    @profile_education = ProfileEducation.new profile_education_params
+    if @profile_education.save
+      flash[:notice] = 'Education added.'
+      redirect_to edit_profile_path(@profile_education.profile)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @profile_education = ProfileEducation.find params[:id]
+    if @profile_education.update_attributes profile_education_params
+      flash[:notice] = 'Changes saved.'
+      redirect_to edit_profile_path(@profile_education.profile)
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def profile_education_params
+    params.require(:profile_education).permit :school,
+                                              :degree,
+                                              :start_year,
+                                              :end_year,
+                                              :field_of_study,
+                                              :description,
+                                              :profile_id,
+                                              :activities_societies
   end
 end
--- app/models/profile_education.rb
+++ app/models/profile_education.rb
@@ -1,3 +1,12 @@
 class ProfileEducation < ActiveRecord::Base
   belongs_to :profile
+
+  validates :school, presence: true
+  validates :degree, presence: true
+  validates :field_of_study, presence: true
+  validates :start_year, numericality: { greater_than: 1900, less_than: 2100 }
+  validates :start_year, numericality: { greater_than: 1900, less_than: 2100 }
+
+  default_scope { order ended_year: :desc, started_year: :desc }
+
 end
