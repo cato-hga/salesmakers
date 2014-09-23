82d4674fff75ae4beda4604b0c140fd8a8973b2f
stephen <milessa42@gmail.com>
2014-09-23 14:19:03 -0400

    Removed routes to non-oneconnect functionality

--- config/routes.rb
+++ config/routes.rb
@@ -58,21 +58,7 @@ Rails.application.routes.draw do
     resources :profile_skills, except: :index
   end
 
-  resources :lines
-  resources :devices do
-    resources :device_deployments, except: [ :index, :new, :create ] do
-      collection do
-        get 'select_user'
-        get 'new/:person_id', to: 'device_deployments#new', as: 'new'
-        post 'new/:person_id', to: 'device_deployments#create'
-      end
 
-      member do
-        get 'end'
-      end
-
-    end
-  end
   resources :log_entries, only: [ :index ]
 
   resources :clients do
@@ -101,37 +87,8 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :themes, except: [:show]
-
   resources :permissions
   resources :permission_groups
-
-  get 'widgets/sales'
-  get 'widgets/sales/people/:person_id', to: 'widgets#person_sales', as: 'person_sales_widget'
-  get 'widgets/hours'
-  get 'widgets/hours/people/:person_id', to: 'widgets#person_hours', as: 'person_hours_widget'
-  get 'widgets/tickets'
-  get 'widgets/tickets/people/:person_id', to: 'widgets#person_tickets', as: 'person_tickets_widget'
-  get 'widgets/social'
-  get 'widgets/alerts'
-  get 'widgets/alerts/people/:person_id', to: 'widgets#person_alerts', as: 'person_alerts_widget'
-  get 'widgets/image_gallery'
-  get 'widgets/inventory'
-  get 'widgets/inventory/people/:person_id', to: 'widgets#person_inventory', as: 'person_inventory_widget'
-  get 'widgets/staffing'
-  get 'widgets/gaming'
-  get 'widgets/commissions'
-  get 'widgets/training'
-  get 'widgets/gift_cards'
-  get 'widgets/gift_cards/people/:person_id', to: 'widgets#person_gift_cards', as: 'person_gift_cards_widget'
-  get 'widgets/pnl'
-  get 'widgets/pnl/people/:person_id', to: 'widgets#person_pnl', as: 'person_pnl_widget'
-  get 'widgets/hps'
-  get 'widgets/hps/people/:person_id', to: 'widgets#person_hps', as: 'person_hps_widget'
-  get 'widgets/assets'
-  get 'widgets/assets/people/:person_id', to: 'widgets#person_assets', as: 'person_assets_widget'
-  get 'widgets/groupme_slider'
-
 
   get 'sessions/destroy', as: 'logout'
 
