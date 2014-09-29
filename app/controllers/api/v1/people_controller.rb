11880436f530946372c55ed02b2cde11f3c0a92c
Anthony Atkinson <aatkinson@retaildoneright.com>
2014-09-29 09:25:32 -0400

    API method for updating person who has changed.

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,5 +1,5 @@
 class API::V1::PeopleController < API::BaseController
-  before_action :get_person, only: [:onboard, :separate]
+  before_action :get_person, only: [:onboard, :separate, :update]
 
   def onboard
     creator = get_creator
@@ -9,6 +9,7 @@ class API::V1::PeopleController < API::BaseController
                     nil,
                     @connect_user.created,
                     @connect_user.created
+      @person.reload
       respond_with @person
     else
       respond_with Person.new
@@ -26,6 +27,47 @@ class API::V1::PeopleController < API::BaseController
                  nil,
                  @connect_user.lastcontact,
                  @connect_user.lastcontact
+    @person.reload
+    respond_with @person
+  end
+
+  def update
+    updater = get_updater
+    new_position = Position.return_from_connect_user(@connect_user)
+    if new_position and @person.position != new_position
+      if @person.update(position: new_position,
+                        updated_at: @connect_user.updated)
+        LogEntry.position_set_from_connect @person,
+                                           updater,
+                                           new_position,
+                                           @connect_user.updated,
+                                           @connect_user.updated
+      end
+    end
+    new_person_area = @person.return_person_area_from_connect
+    return unless new_person_area
+    new_area = new_person_area.area
+    new_leader = new_person_area.manages?
+    old_person_areas = @person.person_areas
+    old_person_area = old_person_areas.count > 0 ? old_person_areas.first : nil
+    old_area = old_person_area ? old_person_area.area : nil
+    old_leader = old_person_area ? old_person_area.manages? : false
+    return unless new_area
+    if new_area != old_area or new_leader != old_leader
+      PersonArea.where(person: @person).destroy_all
+      if new_person_area.save
+        if new_leader
+          LogEntry.assign_as_manager_from_connect @person, updater, new_area, @connect_user.updated, @connect_user.updated
+        else
+          LogEntry.assign_as_employee_from_connect @person, updater, new_area, @connect_user.updated, @connect_user.updated
+        end
+      end
+    end
+    if @person.email != @connect_user.username
+      @person.email = @connect_user.username
+      @person.save
+    end
+    @person.reload
     respond_with @person
   end
 
