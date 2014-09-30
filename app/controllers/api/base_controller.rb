b331857473f2ad04ac784f43a97a14e0bac40806
Anthony Atkinson <aatkinson@retaildoneright.com>
2014-09-30 07:49:22 -0400

    Check IP on API requests.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,11 @@
 class API::BaseController < ApplicationController
-  protect_from_forgery with: :exception
+  before_action :check_ip
   respond_to :json
+  layout false
+
+  def check_ip
+    if request.remote_ip != '184.106.187.251'
+      render file: "public/401.html", status: :unauthorized
+    end
+  end
 end
