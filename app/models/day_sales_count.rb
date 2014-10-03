d1b2d63a75e4e40da4b94e5aaa2abcd8f07e26c1
Anthony Atkinson <aatkinson@retaildoneright.com>
2014-10-03 09:52:45 -0400

    Scopes for date ranges on DaySalesCount.

--- app/models/day_sales_count.rb
+++ app/models/day_sales_count.rb
@@ -7,7 +7,42 @@ class DaySalesCount < ActiveRecord::Base
 
   belongs_to :saleable, polymorphic: true
 
+  scope :today, -> { where day: Date.today }
+  scope :yesterday, -> { where day: Date.yesterday }
+  scope :this_week, -> {
+    where('day >= ? AND day <= ?',
+          Date.today.beginning_of_week,
+          Date.today)
+  }
+  scope :last_week, -> {
+    where('day >= ? AND day <= ?',
+          Date.today.beginning_of_week - 1.week,
+          Date.today.beginning_of_week - 1.day)
+  }
+  scope :this_month, -> {
+    where('day >= ? AND day <= ?',
+          Date.today.beginning_of_month,
+          Date.today)
+  }
+  scope :last_month, -> {
+    where('day >= ? AND day <= ?',
+          Date.today.beginning_of_month - 1.month,
+          Date.today.beginning_of_month - 1.day)
+  }
+  scope :this_year, -> {
+    where('day >= ? AND day <= ?',
+          Date.today.beginning_of_year,
+          Date.today)
+  }
+
+  def for_range(range)
+    where('day >= ? AND day <= ?',
+          range.first,
+          range.last)
+  end
+
   def self.import
     SaleImporter.new
   end
+
 end
