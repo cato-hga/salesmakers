5ac1f7bf02f60dab95ad4970dfde80a477f139d6
stephen <milessa42@gmail.com>
2014-10-09 15:24:33 -0400

    changed wall factory to person_wall

--- spec/factories/walls.rb
+++ spec/factories/walls.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :wall do
+  factory :person_wall, class: Wall do
     wallable_id 1
     wallable_type "Person"
   end
