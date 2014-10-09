00443a565160a82c342f898262bb4466f16d80a6
stephen <milessa42@gmail.com>
2014-10-09 15:25:45 -0400

    Intended behaviors created for wall policy. Hitting wall on polymorphic associations

--- spec/policies/wall_policy_spec.rb
+++ spec/policies/wall_policy_spec.rb
@@ -1,28 +1,34 @@
-# require 'spec_helper'
-#
-# describe WallPolicy do
-#
-#   let(:user) { User.new }
-#
-#   subject { WallPolicy }
-#
-#   permissions ".scope" do
-#     pending "add some examples to (or delete) #{__FILE__}"
-#   end
-#
-#   permissions :create? do
-#     pending "add some examples to (or delete) #{__FILE__}"
-#   end
-#
-#   permissions :show? do
-#     pending "add some examples to (or delete) #{__FILE__}"
-#   end
-#
-#   permissions :update? do
-#     pending "add some examples to (or delete) #{__FILE__}"
-#   end
-#
-#   permissions :destroy? do
-#     pending "add some examples to (or delete) #{__FILE__}"
-#   end
-# end
+require 'rails_helper'
+
+describe WallPolicy do
+
+  describe 'visibility' do
+
+    it 'should include my own wall' do
+    end
+
+    context 'as an HQ employee' do
+      it 'should include my department wall' do
+      end
+
+      it 'should NOT include other department walls' do
+      end
+
+      it 'should include all area and project walls' do
+      end
+    end
+
+    context 'as a field employee' do
+      it 'should include all area walls in my person_area(s)' do
+      end
+
+      it 'should include all ancestors for my area' do
+      end
+    end
+
+    context 'as somebody with all_walls_permission' do
+      it 'should include ALL walls' do
+      end
+    end
+  end
+end
