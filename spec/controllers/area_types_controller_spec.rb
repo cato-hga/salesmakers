606bda5b87310520ec34e35ebd59621165ea629b
Anthony Atkinson <aatkinson@retaildoneright.com>
2014-10-13 14:03:18 -0400

    Adding render_template expectation to AreaTypesController spec.

--- spec/controllers/area_types_controller_spec.rb
+++ spec/controllers/area_types_controller_spec.rb
@@ -8,6 +8,7 @@ describe AreaTypesController do
     it 'returns a success status' do
       get :index, { client_id: project.client.id, project_id: project.id }
       expect(response).to be_success
+      expect(response).to render_template(:index)
     end
   end
 
