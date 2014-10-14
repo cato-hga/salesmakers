987e192f945703ecdaf3537e7606cd24cce1b593
stephen <milessa42@gmail.com>
2014-10-14 11:17:10 -0400

    Test for a person's own wall being visible now passes

--- spec/policies/wall_policy_spec.rb
+++ spec/policies/wall_policy_spec.rb
@@ -4,7 +4,19 @@ describe WallPolicy do
 
   describe 'visibility' do
 
+    let(:client) { create :client }
+    let(:project) { create :project, client: client }
+    let(:territory) { create :area_type, name: 'Test Territory', project: project }
+    let(:area) { create :area, area_type: territory, project: project }
+    let(:field_department) { create :department }
+    let(:field_position) { create :position, department: field_department }
+    let(:field_person) { create :person, position: field_position }
+    let(:person_wall) { create :person_wall}
+    let(:field_person_area) { create :person_area, person: field_person, area: area }
+
     it 'should include my own wall' do
+      field_person.wall = person_wall
+      expect(Pundit.policy_scope(field_person, Wall.all).all).to include(field_person.wall)
     end
 
     context 'as an HQ employee' do
