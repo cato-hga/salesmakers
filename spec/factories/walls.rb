a48d6812215e5a07b8c88c45247059f621c935c2
stephen <milessa42@gmail.com>
2014-10-14 12:12:36 -0400

    HQ employee includes department wall test

--- spec/factories/walls.rb
+++ spec/factories/walls.rb
@@ -3,4 +3,8 @@ FactoryGirl.define do
   factory :person_wall, class: Wall do
     wallable { |w| w.association(:person) }
   end
+
+  factory :department_wall, class: Wall do
+    wallable { |w| w.association(:department) }
+  end
 end
--- spec/policies/wall_policy_spec.rb
+++ spec/policies/wall_policy_spec.rb
@@ -11,16 +11,23 @@ describe WallPolicy do
     let(:field_department) { create :department }
     let(:field_position) { create :position, department: field_department }
     let(:field_person) { create :person, position: field_position }
-    let(:person_wall) { create :person_wall}
+    let(:field_person_wall) { create :person_wall}
     let(:field_person_area) { create :person_area, person: field_person, area: area }
 
+    let(:hq_department) { create :department, name: 'Software Development', corporate: true }
+    let(:software_developer) { build_stubbed :position, name: 'Software Development', hq: true, department: hq_department }
+    let(:hq_department_wall) { create :department_wall }
+    let(:hq_person) { create :person, position: software_developer }
+
     it 'should include my own wall' do
-      field_person.wall = person_wall
+      field_person.wall = field_person_wall
       expect(Pundit.policy_scope(field_person, Wall.all).all).to include(field_person.wall)
     end
 
     context 'as an HQ employee' do
       it 'should include my department wall' do
+        hq_person.department.wall = hq_department_wall
+        expect(Pundit.policy_scope(hq_person, Wall.all).all).to include(hq_department_wall)
       end
 
       it 'should NOT include other department walls' do
