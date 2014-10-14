3e454376c25bea8a51ba1359cf69bb6477cb537d
stephen <milessa42@gmail.com>
2014-10-14 10:21:49 -0400

    new walls factory file

--- spec/factories/walls.rb
+++ spec/factories/walls.rb
@@ -1,8 +1,6 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
+
   factory :person_wall, class: Wall do
-    wallable_id 1
-    wallable_type "Person"
+    wallable { |w| w.association(:person) }
   end
 end
