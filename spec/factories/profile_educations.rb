20036e6a9366541fae44171f0e5e4558de485604
Anthony Atkinson <aatkinson@retaildoneright.com>
2014-10-16 16:30:11 -0400

    ProfileEducationsController spec and clean-up.

--- spec/factories/profile_educations.rb
+++ spec/factories/profile_educations.rb
@@ -1,12 +1,12 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
-# FactoryGirl.define do
-#   factory :profile_education, :class => 'ProfileEducations' do
-#     profile_id 1
-#     school "MyString"
-#     start_year 1
-#     end_year 1
-#     degree "MyString"
-#     field_of_study "MyString"
-#   end
-# end
+FactoryGirl.define do
+  factory :profile_education, :class => 'ProfileEducation' do
+    profile
+    school 'University of South Florida'
+    start_year 2000
+    end_year 2004
+    degree 'Bachelor of Science'
+    field_of_study 'Software Development'
+  end
+end
