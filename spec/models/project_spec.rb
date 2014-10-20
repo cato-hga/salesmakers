7cfb8deb2ead8b1a581e5594119f2513a3895457
stephen <milessa42@gmail.com>
2014-10-20 15:55:30 -0400

    active_people method in Projects now tested

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Project, :type => :model do
   it { should have_many :area_types }
   it { should have_many :areas }
 
+  describe 'active_people method' do
+    before do
+      @project = Project.find_by name: 'Project'
+      area = create :area, project: @project
+      person = create :person
+      person_area = create :person_area, area: area, person: person
+    end
+
+    it 'should return active people for a project' do
+      expect((@project.active_people).count).to eq(1)
+    end
+  end
 end
