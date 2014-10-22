eccdc2c47a30148dfe788f4a7bac447432ef16cf
stephen <milessa42@gmail.com>
2014-10-22 14:09:58 -0400

    Minor tweak wall post comment spec

--- spec/models/wall_post_comment_spec.rb
+++ spec/models/wall_post_comment_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe WallPostComment, :type => :model do
     let(:wall_post_comment) { build_stubbed :wall_post_comment, created_at: Time.now, updated_at: Time.now + 2.minutes }
 
     it 'should return true if the created and updated times differ' do
-      expect(wall_post_comment.is_edited?).to be_truthy
+      expect(wall_post_comment.is_edited?).to eq(true)
     end
   end
 end
