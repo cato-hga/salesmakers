110c6271f12f149533406298a5d8b5ff77dd25bc
stephen <milessa42@gmail.com>
2014-10-27 14:57:37 -0400

    like test started - will refactor to create new method for publication creation.

--- spec/models/group_me_like_spec.rb
+++ spec/models/group_me_like_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe GroupMeLike, :type => :model do
 
   #TODO: Test create_from_json
   #TODO: Test like
+  describe 'liking' do
+    let(:group_me_user) { create :group_me_user }
+    let(:group_me_post) { create :group_me_post }
+
+    it 'should increase the like count' do
+      expect{
+        like = GroupMeLike.like group_me_user, group_me_post
+        group_me_post.reload
+      }.to change(group_me_post.group_me_likes, :count).by(2)
+    end
+  end
 end
