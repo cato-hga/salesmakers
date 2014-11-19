b73bbbaeeead4a3750ccb86b17ec53b05a65b12a
stephen <milessa42@gmail.com>
2014-11-19 14:10:25 -0500

    Reverted changes to deploy for master branch merch

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@rbd.unfuddle.com:rbd/reconnect.git'
 
 set :deploy_to, '/home/deploy'
 set :scm, :git
-set :branch, 'deployment_azure'
+set :branch, 'master'
 set :keep_releases, 5
 
 set :format, :pretty
@@ -111,16 +111,16 @@ end
 # end
 
 namespace :deploy do
-  # desc "Make sure local git is in sync with remote."
-  # task :check_revision do
-  #   on roles(:app) do
-  #     unless `git rev-parse HEAD` == `git rev-parse origin/master`
-  #       puts "WARNING: HEAD is not the same as origin/master"
-  #       puts "Run `git push` to sync changes."
-  #       exit
-  #     end
-  #   end
-  # end
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
 
   desc 'Initial Deploy'
   task :initial do
@@ -137,7 +137,7 @@ namespace :deploy do
     end
   end
 
-  # before :starting,     :check_revision
+  before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
