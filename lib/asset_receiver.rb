6fb9521983d5c6bac13e0400e859d4c0a0d4ca3b
stephen <milessa42@gmail.com>
2014-12-09 16:02:13 -0500

    conctract end date added to receiver

--- lib/asset_receiver.rb
+++ lib/asset_receiver.rb
@@ -1,32 +1,54 @@
 class AssetReceiver
 
-  def initialize(device_model, service_provider, line_identifier, serial, secondary = nil)
-    @device_model = device_model
-    @service_provider = service_provider
-    @line_identifier = line_identifier
-    @serial = serial
-    @secondary = secondary
+  def initialize(options = {})
+    @creator = options.fetch :creator, nil
+    @device_model = options.fetch :device_model, nil
+    @service_provider = options.fetch :service_provider, nil
+    @line_identifier = options.fetch :line_identifier, nil
+    @serial = options.fetch :serial, nil
+    @device_identifier = options.fetch :device_identifier, nil
+    @device_identifier = @serial unless @device_identifier.present?
+    @contract_end_date = options.fetch :contract_end_date, nil
+  end
+
+  def receive
+    line = Line.create identifier: @line_identifier,
+                       contract_end_date: @contract_end_date,
+                       technology_service_provider: @service_provider
+    device = Device.create device_model: @device_model,
+                           serial: @serial,
+                           identifier: @device_identifier
   end
 
   def valid?
-    valid_device_model? and
+    valid_creator? and
+        valid_device_model? and
         valid_service_provider? and
         valid_line_identifier? and
-        valid_serial?
+        valid_serial? and
+        valid_contract_end_date?
+  end
+
+  def valid_creator?
+    @creator.present? and not @creator.new_record?
   end
 
   def valid_device_model?
     @device_model.present? and not @device_model.new_record?
   end
 
+  def create_line?
+    @service_provider.present? or @contract_end_date.present? or @line_identifier.present?
+  end
+
   def valid_service_provider?
-    return false unless @service_provider.present? == @line_identifier.present?
+    return false if create_line? != @service_provider.present?
     return true unless @service_provider.present?
     @service_provider.present? and not @service_provider.new_record?
   end
 
   def valid_line_identifier?
-    return false unless @service_provider.present? == @line_identifier.present?
+    return false if create_line? != @line_identifier.present?
     if @line_identifier.present?
       @line_identifier = @line_identifier.gsub(/[^0-9A-Za-z]/, '')
       @line_identifier.length == 10
@@ -35,7 +57,17 @@ class AssetReceiver
     end
   end
 
+  def valid_contract_end_date?
+    return false if create_line? != @contract_end_date.present?
+    return true if not create_line?
+    @contract_end_date.is_a? Date
+  end
+
   def valid_serial?
     @serial.present? and @serial.length >= 6
+  end
+
+  def device_identifier
+    @device_identifier
   end
 end
--- spec/libs/asset_receiver_spec.rb
+++ spec/libs/asset_receiver_spec.rb
@@ -2,80 +2,100 @@ require 'rails_helper'
 
 RSpec.describe 'Asset Receiver' do
 
+  before {
+    @attrs = {
+        creator: creator,
+        device_model: device_model,
+        service_provider: service_provider,
+        line_identifier: line_identifier,
+        serial: serial,
+        device_identifier: device_identifier,
+        contract_end_date: Date.today + 1.year
+    }
+  }
   let(:device_model) { build_stubbed :device_model }
   let(:service_provider) { build_stubbed :technology_service_provider }
   let(:line_identifier) { '5555555555' }
   let(:serial) { '123456789' }
-  let(:secondary) { '98765431' }
-  let(:receiver) { AssetReceiver.new device_model,
-                                     service_provider,
-                                     line_identifier,
-                                     serial,
-                                     secondary }
+  let(:device_identifier) { '98765431' }
+  let(:creator) { build_stubbed :person }
+  let(:receiver) { AssetReceiver.new @attrs }
 
   it 'is valid with the correct inputs' do
     expect(receiver).to be_valid
   end
 
   it 'is invalid when service provider is present but line identifier is not' do
-    invalid_receiver = AssetReceiver.new device_model,
-                                         service_provider,
-                                         nil,
-                                         serial,
-                                         secondary
-    expect(invalid_receiver).not_to be_valid_service_provider
+    invalid_receiver = AssetReceiver.new @attrs.merge(line_identifier: nil)
     expect(invalid_receiver).not_to be_valid_line_identifier
     expect(invalid_receiver).not_to be_valid
   end
 
   it 'is invalid when line identifier is present but service provider is not' do
-    invalid_receiver = AssetReceiver.new device_model,
-                                         nil,
-                                         line_identifier,
-                                         serial,
-                                         secondary
+    invalid_receiver = AssetReceiver.new @attrs.merge(service_provider: nil)
     expect(invalid_receiver).not_to be_valid_service_provider
+    expect(invalid_receiver).not_to be_valid
+  end
+
+  it 'is invalid when a contract end date is present but line_identifier is not' do
+    invalid_receiver = AssetReceiver.new @attrs.merge(line_identifier: nil)
     expect(invalid_receiver).not_to be_valid_line_identifier
     expect(invalid_receiver).not_to be_valid
   end
 
+  it 'is invalid when a line identifier is present but a contract end date is not' do
+    invalid_receiver = AssetReceiver.new @attrs.merge(contract_end_date: nil)
+    expect(invalid_receiver).not_to be_valid_contract_end_date
+    expect(invalid_receiver).not_to be_valid
+  end
+
   it 'requires a serial' do
-    invalid_receiver = AssetReceiver.new device_model,
-                                         service_provider,
-                                         line_identifier,
-                                         nil,
-                                         secondary
+    invalid_receiver = AssetReceiver.new @attrs.merge(serial: nil)
     expect(invalid_receiver).not_to be_valid_serial
     expect(invalid_receiver).not_to be_valid
   end
 
   it 'requires a device model' do
-    invalid_receiver = AssetReceiver.new nil,
-                                         service_provider,
-                                         line_identifier,
-                                         serial,
-                                         secondary
+    invalid_receiver = AssetReceiver.new @attrs.merge(device_model: nil)
     expect(invalid_receiver).not_to be_valid_device_model
     expect(invalid_receiver).not_to be_valid
   end
 
-  it 'allows nil values for line and service provider' do
-    valid_receiver = AssetReceiver.new device_model,
-                                       nil,
-                                       nil,
-                                       serial,
-                                       secondary
+  it 'allows nil values for line and service provider and contract end date' do
+    valid_receiver = AssetReceiver.new @attrs.merge(line_identifier: nil, service_provider: nil, contract_end_date: nil)
     expect(valid_receiver).to be_valid_service_provider
+    expect(valid_receiver).to be_valid_contract_end_date
     expect(valid_receiver).to be_valid_line_identifier
     expect(valid_receiver).to be_valid
   end
 
-  it 'creates a device'
-  it 'creates a line'
+  it 'requires a creator (person)' do
+    invalid_receiver = AssetReceiver.new @attrs.merge(creator: nil)
+    expect(invalid_receiver).not_to be_valid_creator
+    expect(invalid_receiver).not_to be_valid
+  end
+
+  it 'sets the device identifier if provided' do
+    expect(receiver.device_identifier).to eq(device_identifier)
+  end
+
+  it 'uses the devices serial as device_identifier if no identifier is provided' do
+    receiver = AssetReceiver.new @attrs.merge(device_identifier: nil)
+    expect(receiver.device_identifier).to eq(serial)
+  end
+
+  it 'creates a device' do
+    expect { receiver.receive }.to change(Device, :count).by(1)
+  end
+
+  it 'creates a line' do
+    expect { receiver.receive }.to change(Line, :count).by(1)
+  end
+
   it 'creates a log entry for receiving'
   it 'sets the proper service provider'
   it 'sets the proper manufacturer and model'
   it 'assigns the proper device serial'
   it 'assigns the proper device line'
-  it 'assigns the proper device secondary identifier, if required'
+  it 'assigns the proper device device_identifier identifier, if required'
 end
