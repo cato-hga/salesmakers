895c341f1ba72dc0c030bc076e6324829b98490a
stephen <milessa42@gmail.com>
2014-12-22 16:28:53 -0500

    Changes to contract end date and line display broke a few tests - these are now fixed

--- spec/features/lines/lines_crud_spec.rb
+++ spec/features/lines/lines_crud_spec.rb
@@ -20,7 +20,35 @@ describe 'Lines CRUD actions' do
     let(:line) { create :line }
     before { visit line_path line }
     it 'displays the contract end date' do
-      expect(page).to have_content(line.contract_end_date)
+      expect(page).to have_content(line.contract_end_date.strftime '%m/%d/%Y')
+    end
+  end
+
+  describe 'PATCH update' do
+    let(:old_line) { create :line }
+    let!(:new_line) { create :line, identifier: '5555555555' }
+    let(:device) { create :device, line: old_line }
+    subject {
+      visit swap_lines_path
+      within('#lines') do
+        within all('.row').first do
+          find('.serial_field').set(device.serial)
+          find('.line_field').set(new_line.identifier)
+        end
+      end
+      click_on 'Swap'
+    }
+    it 'swaps the line' do
+      subject
+      device.reload
+      expect(device.line).to eq(new_line)
+      expect(device.line).not_to eq(old_line)
+    end
+
+    it 'redirects to the lines index page' do
+      subject
+      expect(page).to have_content('(555) 555-5555')
+      expect(page).not_to have_content('New Line')
     end
   end
 end
