a0821fd845f17621bbc6bfc4f278f09ac771b665
stephen <milessa42@gmail.com>
2014-12-24 10:25:58 -0500

    Aww boo that didn't work. Hardcoded asset_management branch in.

--- config/deploy.rb
+++ config/deploy.rb
@@ -114,8 +114,8 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
-        puts "WARNING: HEAD is not the same as origin/#{branch}"
+      unless `git rev-parse HEAD` == `git rev-parse origin/asset_management`
+        puts "WARNING: HEAD is not the same as origin/asset_management"
         puts "Run `git push` to sync changes."
         exit
       end
