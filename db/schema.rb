51e8eabd801e1f4bd28a10c930e0bddeff389474
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-01-13 10:53:29 -0500

    twilio_number recorded on Position in order to know where to send incoming messages that are not replies to any other message.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150113123125) do
+ActiveRecord::Schema.define(version: 20150113154649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -406,6 +406,7 @@ ActiveRecord::Schema.define(version: 20150113123125) do
     t.datetime "updated_at"
     t.boolean  "field"
     t.boolean  "hq"
+    t.string   "twilio_number"
   end
 
   add_index "positions", ["department_id"], name: "index_positions_on_department_id", using: :btree
