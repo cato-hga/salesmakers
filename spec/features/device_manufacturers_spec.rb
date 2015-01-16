a125ee10a153e79f64bd144fab8289520447a1de
stephen <milessa42@gmail.com>
2015-01-16 14:01:12 -0500

    missed commit

--- spec/features/device_manufacturers_spec.rb
+++ spec/features/device_manufacturers_spec.rb
@@ -10,5 +10,23 @@ feature "DeviceManufacturers", :type => :feature do
     end
   end
 
-  describe 'POST create'
+  describe 'POST create' do
+    before(:each) do
+      visit new_device_manufacturer_path
+      fill_in 'device_manufacturer_name', with: 'Test Manufacturer'
+      click_on 'Submit'
+    end
+
+    it 'creates a new device model' do
+      expect(page).to have_content('Test Manufacturer')
+    end
+
+    it 'flashes a success message' do
+      expect(page).to have_content('Device manufacturer created')
+    end
+
+    it 'redirects to the new device model path' do
+      expect(page).to have_content('New Device Model')
+    end
+  end
 end
