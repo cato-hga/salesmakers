39f7551340220476f05d26ad5ca963b8d63c3dfb
stephen <milessa42@gmail.com>
2015-01-20 14:17:05 -0500

    FOUND THE FUCKING TYPO --skip-ci

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,14 +39,13 @@ namespace :puma do
 end
 
 namespace :staging do
-  desc 'Sync staging and production database' do
-    task :sync do
-      on roles(:app) do
-        execute 'pg_dump -Fc -h 10.209.169.144 -U oneconnect -f /tmp/dbdump.dump oneconnect_production'
-        execute 'dropdb -U oneconnect oneconnect_production'
-        execute 'createdb -U oneconnect -O oneconnect oneconnect_production'
-        execute 'pg_restore -j 4 -v -d oneconnect_production -U oneconnect /tmp/dbdump.dump'
-      end
+  desc 'Sync staging and production database'
+  task :sync do
+    on roles(:app) do
+      execute 'pg_dump -Fc -h 10.209.169.144 -U oneconnect -f /tmp/dbdump.dump oneconnect_production'
+      execute 'dropdb -U oneconnect oneconnect_production'
+      execute 'createdb -U oneconnect -O oneconnect oneconnect_production'
+      execute 'pg_restore -j 4 -v -d oneconnect_production -U oneconnect /tmp/dbdump.dump'
     end
   end
 end
