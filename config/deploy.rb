4493ad92d509194395c6640f9051ffec13af2bf7
stephen <milessa42@gmail.com>
2015-01-22 09:53:42 -0500

    Capfile now locked at 3.3.5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
+# config valid only for Capistrano 3.3.5
+lock '3.3.5'
 
 set :application, 'oneconnect'
 set :repo_url, 'git@bitbucket.org:salesmakers/salesmakers.git'
