aaab3c2603daae604974b33afcf954db02790bb3
stephen <milessa42@gmail.com>
2015-01-24 11:22:28 -0500

    decoupled factories from adminstrator_person

--- spec/features/device_states_crud_spec.rb
+++ spec/features/device_states_crud_spec.rb
@@ -2,20 +2,36 @@ require 'rails_helper'
 
 describe 'DeviceStates CRUD actions' do
 
+  let!(:person) { create :person, position: position, email: 'ittech@salesmakersinc.com' }
+  let(:permission_index) { create :permission, key: 'device_state_index' }
+  let(:permission_new) { create :permission, key: 'device_state_new' }
+  let(:permission_update) { create :permission, key: 'device_state_update' }
+  let(:permission_edit) { create :permission, key: 'device_state_edit' }
+  let(:permission_destroy) { create :permission, key: 'device_state_destroy' }
+  let(:permission_create) { create :permission, key: 'device_state_create' }
+  let(:position) { create :position, name: 'IT Tech', department: department }
+  let(:department) { create :department, name: 'Information Technology' }
+
+  before(:each) do
+    CASClient::Frameworks::Rails::Filter.fake("ittech@salesmakersinc.com")
+  end
+
   context 'for creating' do
     let(:device_state) { build :device_state }
-
-    subject {
+    before(:each) do
+      position.permissions << permission_index
+      position.permissions << permission_create
       visit device_states_path
       within '#main_container h1' do
         click_on 'new_action_button'
       end
+    end
+    subject {
       fill_in 'Name', with: device_state.name
       click_on 'Save'
     }
 
     it 'has the correct page title' do
-      visit new_device_state_path
       expect(page).to have_selector('h1', text: 'New Device State')
     end
 
@@ -31,8 +47,11 @@ describe 'DeviceStates CRUD actions' do
 
   context 'for reading' do
     let!(:device_state) { create :device_state }
+    let(:permission_device_index) { create :permission, key: 'device_index' }
 
     it 'navigates to the device states index' do
+      position.permissions << permission_index
+      position.permissions << permission_device_index
       visit devices_path
       within '#main_container header h1' do
         click_on 'Edit States'
@@ -47,6 +66,9 @@ describe 'DeviceStates CRUD actions' do
       let(:new_name) { 'New Name' }
 
       subject {
+        position.permissions << permission_index
+        position.permissions << permission_new
+        position.permissions << permission_update
         visit device_states_path
         click_on device_state.name
         fill_in 'Name', with: new_name
@@ -79,6 +101,9 @@ describe 'DeviceStates CRUD actions' do
     let(:device) { create :device }
 
     subject do
+      position.permissions << permission_index
+      position.permissions << permission_destroy
+      position.permissions << permission_update
       visit device_states_path
       click_on device_state.name
       page.driver.browser.accept_js_confirms
