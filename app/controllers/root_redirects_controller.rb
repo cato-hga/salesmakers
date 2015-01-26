3e275e429e46343df0369a8c38a9576e19726441
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-01-26 11:48:27 -0500

    Coming soon page for unimplemented departments.

--- app/controllers/root_redirects_controller.rb
+++ app/controllers/root_redirects_controller.rb
@@ -3,10 +3,11 @@ class RootRedirectsController < ApplicationController
   def incoming_redirect
     department = @current_person.position.department
     if department.name == 'Information Technology'
-      redirect_to devices_path
+      redirect_to devices_path and return
     end
     if department.name == 'Comcast Retail Sales'
-      redirect_to new_comcast_customer_path
+      redirect_to new_comcast_customer_path and return
     end
+
   end
 end
