fefe7e863432e0ccf4f86e6fbb6cf952e261967b
stephen <milessa42@gmail.com>
2015-01-26 12:13:50 -0500

    trying no puma:stop/start --skip-ci

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,12 +43,12 @@ namespace :staging do
   desc 'Sync staging and production database'
   task :sync do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:stop'
+      #invoke 'puma:stop'
       execute 'pg_dump -Fc -h 10.209.169.144 -U oneconnect -f /tmp/dbdump.dump oneconnect_production'
       execute 'dropdb -U oneconnect oneconnect_production'
       execute 'createdb -U oneconnect -O oneconnect oneconnect_production'
       execute 'pg_restore -j 4 -v -d oneconnect_production -U oneconnect /tmp/dbdump.dump'
-      invoke 'puma:start'
+      #invoke 'puma:start'
     end
   end
 end
