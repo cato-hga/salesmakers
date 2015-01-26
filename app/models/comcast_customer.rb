0aa5dfde07d223ac74610ea5a75067d7ce37b7f5
stephen <milessa42@gmail.com>
2015-01-26 12:02:29 -0500

    validations on comcast customer and sale

--- app/models/comcast_customer.rb
+++ app/models/comcast_customer.rb
@@ -1,2 +1,9 @@
 class ComcastCustomer < ActiveRecord::Base
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :person_id, presence: true
+
+  belongs_to :person
+
 end
