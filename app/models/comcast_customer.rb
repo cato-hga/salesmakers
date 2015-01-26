b0b29df6218880f267fe34ad0ed789bda822c055
stephen <milessa42@gmail.com>
2015-01-26 12:26:26 -0500

    added phone validatons to comcast customer

--- app/models/comcast_customer.rb
+++ app/models/comcast_customer.rb
@@ -1,8 +1,10 @@
+require 'validators/phone_number_validator'
 class ComcastCustomer < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :person_id, presence: true, uniqueness: true
+  validates_with PhoneNumberValidator
 
   belongs_to :person
 
