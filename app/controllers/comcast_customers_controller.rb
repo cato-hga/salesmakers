bc4f46f15060b19d3cc09daa7c176c17fb4f3414
stephen <milessa42@gmail.com>
2015-01-26 16:00:27 -0500

    Initial tests pass

--- app/controllers/comcast_customers_controller.rb
+++ app/controllers/comcast_customers_controller.rb
@@ -1,10 +1,27 @@
 class ComcastCustomersController < ApplicationController
+  before_action :do_authorization
   after_action :verify_authorized
+
 
   def new
     authorize ComcastCustomer.new
   end
 
   def create
+    @comcast_customer = ComcastCustomer.new customer_params
+    if @comcast_customer.save
+      @current_person.log? 'create', @comcast_customer, @current_person
+      #Redirection pending COMcastSales
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:comcast_customer).permit(:first_name, :last_name, :mobile_phone, :other_phone, :person_id, :comments)
+  end
+
+  def do_authorization
+    authorize ComcastCustomer.new
   end
 end
--- spec/controllers/comcast_customers_controller_spec.rb
+++ spec/controllers/comcast_customers_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
-describe DeviceManufacturersController, :type => :controller do
+describe ComcastCustomersController do
 
   describe 'GET new' do
     before(:each) do
+      allow(controller).to receive(:policy).and_return double(new?: true)
       get :new
     end
     it 'returns a success status' do
@@ -11,6 +12,38 @@ describe DeviceManufacturersController, :type => :controller do
     end
     it 'renders the index template' do
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST create' do
+    let(:new_customer) { build :comcast_customer }
+    let(:person) { create :it_tech_person }
+    before(:each) do
+      CASClient::Frameworks::Rails::Filter.fake("ittech@salesmakersinc.com")
+      allow(controller).to receive(:policy).and_return double(create?: true)
+    end
+    context 'success' do
+      subject do
+        post :create,
+             comcast_customer: {
+                 first_name: new_customer.first_name,
+                 last_name: new_customer.last_name,
+                 mobile_phone: new_customer.mobile_phone,
+                 person_id: person.id
+             }
+      end
+
+      it 'creates a comcast customer' do
+        expect { subject }.to change(ComcastCustomer, :count).by(1)
+      end
+
+      it 'creates a log entry' do
+        expect { subject }.to change(LogEntry, :count).by(1)
+      end
+
+      it 'redirects to comcast_sale#new', pending: 'Comcast Sales not created' do
+        expect(subject).to redirect_to(new_comcast_sales_path)
+      end
     end
   end
 end
