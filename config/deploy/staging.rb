a1db5cfb8acac788cf30881cef385002f204c88c
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-01-26 10:49:46 -0500

    Getting old --skip-ci

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,12 +1,12 @@
 set :stage, :staging
 set :rails_env, 'staging'
 set :puma_env, 'staging'
-# set :puma_init_active_record, true
-# set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_access_log, "#{release_path}/log/puma.error.log"
-# set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :puma_init_active_record, true
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log, "#{release_path}/log/puma.access.log"
 
 role :app, %w{deploy@staging.rbdconnect.com}
 role :web, %w{deploy@staging.rbdconnect.com}
