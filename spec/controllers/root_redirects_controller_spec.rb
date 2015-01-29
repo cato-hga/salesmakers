f0da7be8e7ce3220922dccf6e9855310d1923439
stephen <milessa42@gmail.com>
2015-01-29 16:28:02 -0500

    Executives now get sent to people#index and only have access to that so far. --skip-ci

--- spec/controllers/root_redirects_controller_spec.rb
+++ spec/controllers/root_redirects_controller_spec.rb
@@ -4,17 +4,14 @@ describe RootRedirectsController do
 
   describe 'GET incoming_redirect' do
 
-    context 'IT Department' do
+    describe 'IT Department' do
       let!(:it_employee) { create :person, position: position, email: 'ittech@salesmakersinc.com' }
       let(:permissions) { create :permission }
       let(:position) { create :position, name: 'IT Tech', department: department }
       let(:department) { create :department, name: 'Information Technology' }
       before(:each) do
-        CASClient::Frameworks::Rails::Filter.fake("ittech@salesmakersinc.com")
+        CASClient::Frameworks::Rails::Filter.fake(it_employee.email)
         get :incoming_redirect
-      end
-      after(:each) do
-        CASClient::Frameworks::Rails::Filter.fake("retailingw@retaildoneright.com")
       end
       it 'returns a redirect status' do
         expect(response).to be_redirect
@@ -24,17 +21,30 @@ describe RootRedirectsController do
       end
     end
 
+    describe 'Executives' do
+      let!(:executive) { create :person, position: position, email: 'executive@salesmakersinc.com' }
+      let(:position) { create :position, name: 'Executives', department: department }
+      let(:department) { create :department, name: 'Executives' }
+      before(:each) do
+        CASClient::Frameworks::Rails::Filter.fake(executive.email)
+        get :incoming_redirect
+      end
+      it 'returns a redirect status' do
+        expect(response).to be_redirect
+      end
+      it 'routes Executives to the people index' do
+        expect(response).to redirect_to(people_path)
+      end
+    end
 
-    context 'Comcast Sales' do
+
+    describe 'Comcast Sales' do
       let!(:comcast_employee) { create :person, position: comcast_position, email: 'comcastemployee@cc.salesmakersinc.com' }
       let(:comcast_position) { create :position, name: 'Comcast Sales Specialist', department: comcast_department }
       let(:comcast_department) { create :department, name: 'Comcast Retail Sales' }
       before(:each) do
-        CASClient::Frameworks::Rails::Filter.fake("comcastemployee@cc.salesmakersinc.com")
+        CASClient::Frameworks::Rails::Filter.fake(comcast_employee.email)
         get :incoming_redirect
-      end
-      after(:each) do
-        CASClient::Frameworks::Rails::Filter.fake("retailingw@retaildoneright.com")
       end
       it 'returns a redirect' do
         expect(response).to be_redirect
