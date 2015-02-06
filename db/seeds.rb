e65844800130c0b0424957bb34fcb08a9aacd6e3
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-02-06 08:06:31 -0500

    OK GODDAMNIT --skip-ci

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@ require "#{seeds}/department"
 require "#{seeds}/position"
 require "#{seeds}/permissions"
 require "#{seeds}/administrators"
-User.find_by_email('retailingw@retaildoneright.com').make_current
 
 require "#{seeds}/clients"
 require "#{seeds}/projects"
