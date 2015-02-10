fce14db4697fc4d370a2b56abd758ee85f6b35b1
stephen <milessa42@gmail.com>
2015-02-10 10:12:01 -0500

    change action mailer default host.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'staging.rbdconnect.com'}
+  config.action_mailer.default_url_options = {host: 'staging.salesmakersinc.com'}
   config.middleware.use ExceptionNotification::Rack,
                         email: {
                             email_prefix: '[SC2.0 - TESTING] ',
