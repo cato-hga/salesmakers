f39f8dd0cde787fb3a4dc8960be3b1e47da269a7
stephen <milessa42@gmail.com>
2015-02-13 17:01:19 -0500

    Comcast EOD's now creatable

--- app/controllers/comcast_eods_controller.rb
+++ app/controllers/comcast_eods_controller.rb
@@ -7,9 +7,26 @@ class ComcastEodsController < ApplicationController
   end
 
   def create
+    @comcast_eod = ComcastEod.new comcast_eod_params
+    if @comcast_eod.save
+      redirect_to comcast_customers_path
+    end
   end
 
   private
+
+  def comcast_eod_params
+    params.require(:comcast_eod).permit(:location_id,
+                                        :eod_date,
+                                        :sales_pro_visit,
+                                        :sales_pro_visit_takeaway,
+                                        :comcast_visit,
+                                        :comcast_visit_takeaway,
+                                        :cloud_training,
+                                        :cloud_training_takeaway
+
+    )
+  end
 
   def do_authorization
     authorize ComcastCustomer.new
--- spec/controllers/comcast_eods_controller_spec.rb
+++ spec/controllers/comcast_eods_controller_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 describe ComcastEodsController do
+  let(:person) { create :comcast_employee, position: position }
+  let(:position) { create :comcast_sales_position }
+  let!(:project) { create :project, name: 'Comcast Retail' }
 
-  before { CASClient::Frameworks::Rails::Filter.fake(comcast_employee.email) }
+  before(:each) do
+    allow(controller).to receive(:policy).and_return double(new?: true,
+                                                            index?: true,
+                                                            create?: true)
+    CASClient::Frameworks::Rails::Filter.fake(person.email)
+  end
 
-  describe 'GET new', pending: 'pending' do
+  describe 'GET new' do
     before {
-      allow(controller).to receive(:policy).and_return double(new?: true)
-      get :new, comcast_customer_id: comcast_customer.id }
+      get :new
+    }
 
     it 'returns a success status' do
       expect(response).to be_success
@@ -18,21 +26,47 @@ describe ComcastEodsController do
     end
   end
 
-  describe 'POST create', pending: 'pending' do
+  describe 'POST create' do
+    let(:location) { create :location }
+    let(:person_area) { create :person_area,
+                               person: person,
+                               area: create(:area,
+                                            project: project) }
+    let!(:location_area) { create :location_area,
+                                  location: location,
+                                  area: person_area.area }
+    let!(:eod_date) { DateTime.now }
     context 'success' do
       before do
         post :create,
-             comcast_customer_id: comcast_customer.id,
-             comcast_lead: {
-                 tv: true,
-                 ok_to_call_and_text: true
+             comcast_eod: {
+                 eod_date: eod_date,
+                 location_id: location.id,
+                 sales_pro_visit: true,
+                 sales_pro_visit_takeaway: 'Sales Pro Takeaway',
+                 comcast_visit: true,
+                 comcast_visit_takeaway: 'Comcast Visit Takeaway',
+                 cloud_training: true,
+                 cloud_training_takeaway: 'Cloud Training Takeaway',
              }
       end
 
       it 'should redirect to ComcastCustomers#index' do
         expect(response).to redirect_to(comcast_customers_path)
       end
+
+      it 'assigns proper attributes' do
+        eod = ComcastEod.first
+        expect(eod.location).to eq(location)
+        expect(eod.sales_pro_visit).to eq(true)
+        expect(eod.sales_pro_visit_takeaway).to eq('Sales Pro Takeaway')
+        expect(eod.comcast_visit).to eq(true)
+        expect(eod.comcast_visit_takeaway).to eq('Comcast Visit Takeaway')
+        expect(eod.cloud_training).to eq(true)
+        expect(eod.cloud_training_takeaway).to eq('Cloud Training Takeaway')
+      end
     end
+
     context 'failure' do
 
       it 'redirects to ComcastEOD#new'
