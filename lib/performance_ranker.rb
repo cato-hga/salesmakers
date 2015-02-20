b8cada96987f55960a9f9942bf3be5816dd0ec68
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-02-20 15:35:22 -0500

    Removing complexity from PerformanceRanker.

--- lib/performance_ranker.rb
+++ lib/performance_ranker.rb
@@ -3,111 +3,61 @@ class PerformanceRanker
   def self.rank_people_sales
     for project in Project.all do
       ((Date.today - 1.month)..(Date.today)).each do |day|
-        day_people_sales = DaySalesCount.find_by_sql("
-            SELECT
-
-            day_sales_counts.*
-
-            FROM day_sales_counts
-            LEFT OUTER JOIN people
-              ON people.id = day_sales_counts.saleable_id
-            LEFT OUTER JOIN person_areas
-              ON person_areas.person_id = people.id
-            LEFT OUTER JOIN areas
-              ON areas.id = person_areas.area_id
-            LEFT OUTER JOIN projects
-              ON projects.id = areas.project_id
-
-            WHERE
-              projects.id = " + project.id.to_s + "
-              AND day_sales_counts.saleable_type = 'Person'
-              AND day_sales_counts.day = CAST('" +
-                                                         day.strftime('%m/%d/%Y') + "' AS DATE)
-
-            ORDER BY day_sales_counts.sales DESC")
-        process_day_rankings day_people_sales, day
-        week_people_sales_sql = "SELECT
-
-            day_sales_counts.saleable_id as id,
-            sum(day_sales_counts.sales) AS sales
-
-            FROM day_sales_counts
-            LEFT OUTER JOIN people
-              ON people.id = day_sales_counts.saleable_id
-            LEFT OUTER JOIN person_areas
-              ON person_areas.person_id = people.id
-            LEFT OUTER JOIN areas
-              ON areas.id = person_areas.area_id
-            LEFT OUTER JOIN projects
-              ON projects.id = areas.project_id
-
-            WHERE
-              projects.id = " + project.id.to_s + "
-              AND day_sales_counts.saleable_type = 'Person'
-              AND day_sales_counts.day >= CAST('" +
-                  day.beginning_of_week.strftime('%m/%d/%Y') + "' AS DATE)
-              AND day_sales_counts.day <= CAST('" +
-                  day.strftime('%m/%d/%Y') + "' AS DATE)
-
-            GROUP BY day_sales_counts.saleable_id
-            ORDER BY sum(day_sales_counts.sales) DESC, day_sales_counts.saleable_id"
-        week_people_sales = ActiveRecord::Base.connection.execute(week_people_sales_sql)
-        process_week_rankings week_people_sales, day, 'Person'
-        month_people_sales_sql = "SELECT
-
-            day_sales_counts.saleable_id as id,
-            sum(day_sales_counts.sales) AS sales
-
-            FROM day_sales_counts
-            LEFT OUTER JOIN people
-              ON people.id = day_sales_counts.saleable_id
-            LEFT OUTER JOIN person_areas
-              ON person_areas.person_id = people.id
-            LEFT OUTER JOIN areas
-              ON areas.id = person_areas.area_id
-            LEFT OUTER JOIN projects
-              ON projects.id = areas.project_id
-
-            WHERE
-              projects.id = " + project.id.to_s + "
-              AND day_sales_counts.saleable_type = 'Person'
-              AND day_sales_counts.day >= CAST('" +
-            day.beginning_of_month.strftime('%m/%d/%Y') + "' AS DATE)
-              AND day_sales_counts.day <= CAST('" +
-            day.strftime('%m/%d/%Y') + "' AS DATE)
-
-            GROUP BY day_sales_counts.saleable_id
-            ORDER BY sum(day_sales_counts.sales) DESC, day_sales_counts.saleable_id"
-        month_people_sales = ActiveRecord::Base.connection.execute(month_people_sales_sql)
-        process_month_rankings month_people_sales, day, 'Person'
+        day_people_sales = query_people_sales(project, day, day)
+        process_rankings day_people_sales, day, :day_rank, 'Person'
+        week_people_sales = query_people_sales(project, day.beginning_of_week, day)
+        process_rankings week_people_sales, day, :week_rank, 'Person'
+        month_people_sales = query_people_sales(project, day.beginning_of_month, day)
+        process_rankings month_people_sales, day, :month_rank, 'Person'
       end
     end
+  end
+
+  def self.query_people_sales(project, start_date, end_date)
+    ActiveRecord::Base.connection.execute %{
+            SELECT
+
+            day_sales_counts.saleable_id AS id,
+            sum(day_sales_counts.sales) AS sales
+
+            FROM day_sales_counts
+            LEFT OUTER JOIN people
+              ON people.id = day_sales_counts.saleable_id
+            LEFT OUTER JOIN person_areas
+              ON person_areas.person_id = people.id
+            LEFT OUTER JOIN areas
+              ON areas.id = person_areas.area_id
+            LEFT OUTER JOIN projects
+              ON projects.id = areas.project_id
+
+            WHERE
+              projects.id = #{project.id.to_s}
+              AND day_sales_counts.saleable_type = 'Person'
+              AND day_sales_counts.day >= CAST('#{start_date.strftime('%m/%d/%Y')}' AS DATE)
+              AND day_sales_counts.day <= CAST('#{end_date.strftime('%m/%d/%Y')}' AS DATE)
+
+            GROUP BY day_sales_counts.saleable_id
+            ORDER BY sum(day_sales_counts.sales) DESC, day_sales_counts.saleable_id
+    }
   end
 
   def self.rank_areas_sales
     for project in Project.all do
       ((Date.today - 1.month)..(Date.today)).each do |day|
-        day_areas_sales = DaySalesCount.find_by_sql("
+        day_areas_sales = query_area_sales(project, day, day)
+        process_rankings day_areas_sales, day, :day_rank, 'Area'
+        week_areas_sales = query_area_sales(project, day.beginning_of_week, day)
+        process_rankings week_areas_sales, day, :week_rank, 'Area'
+        month_areas_sales = query_area_sales(project, day.beginning_of_month, day)
+        process_rankings month_areas_sales, day, :month_rank, 'Area'
+      end
+    end
+  end
+
+  def self.query_area_sales(project, start_date, end_date)
+    ActiveRecord::Base.connection.execute %{
             SELECT
 
-            day_sales_counts.*
-
-            FROM day_sales_counts
-            LEFT OUTER JOIN areas
-              ON areas.id = day_sales_counts.saleable_id
-            LEFT OUTER JOIN projects
-              ON projects.id = areas.project_id
-
-            WHERE
-              projects.id = " + project.id.to_s + "
-              AND day_sales_counts.saleable_type = 'Area'
-              AND day_sales_counts.day = CAST('" +
-                                                        day.strftime('%m/%d/%Y') + "' AS DATE)
-
-            ORDER BY day_sales_counts.sales DESC")
-        process_day_rankings day_areas_sales, day
-        week_areas_sales_sql = "SELECT
-
             day_sales_counts.saleable_id as id,
             sum(day_sales_counts.sales) AS sales
 
@@ -118,93 +68,29 @@ class PerformanceRanker
               ON projects.id = areas.project_id
 
             WHERE
-              projects.id = " + project.id.to_s + "
+              projects.id = #{project.id.to_s}
               AND day_sales_counts.saleable_type = 'Area'
-              AND day_sales_counts.day >= CAST('" +
-            day.beginning_of_week.strftime('%m/%d/%Y') + "' AS DATE)
-              AND day_sales_counts.day <= CAST('" +
-            day.strftime('%m/%d/%Y') + "' AS DATE)
+              AND day_sales_counts.day >= CAST('#{start_date.strftime('%m/%d/%Y')}' AS DATE)
+              AND day_sales_counts.day <= CAST('#{end_date.strftime('%m/%d/%Y')}' AS DATE)
 
             GROUP BY day_sales_counts.saleable_id
             ORDER BY sum(day_sales_counts.sales) DESC, day_sales_counts.saleable_id"
-        week_areas_sales = ActiveRecord::Base.connection.execute(week_areas_sales_sql)
-        process_week_rankings week_areas_sales, day, 'Area'
-        month_areas_sales_sql = "SELECT
-
-            day_sales_counts.saleable_id as id,
-            sum(day_sales_counts.sales) AS sales
-
-            FROM day_sales_counts
-            LEFT OUTER JOIN areas
-              ON areas.id = day_sales_counts.saleable_id
-            LEFT OUTER JOIN projects
-              ON projects.id = areas.project_id
-
-            WHERE
-              projects.id = " + project.id.to_s + "
-              AND day_sales_counts.saleable_type = 'Area'
-              AND day_sales_counts.day >= CAST('" +
-            day.beginning_of_month.strftime('%m/%d/%Y') + "' AS DATE)
-              AND day_sales_counts.day <= CAST('" +
-            day.strftime('%m/%d/%Y') + "' AS DATE)
-
-            GROUP BY day_sales_counts.saleable_id
-            ORDER BY sum(day_sales_counts.sales) DESC, day_sales_counts.saleable_id"
-        month_areas_sales = ActiveRecord::Base.connection.execute(month_areas_sales_sql)
-        process_month_rankings month_areas_sales, day, 'Area'
-      end
-    end
+        }
   end
 
-  def self.day_sales_counts(project, day, type)
-
-  end
-
-  def self.process_day_rankings(day_sales_counts, day)
+  def self.process_rankings(sales_counts, day, column, type)
     rank = 0
     sales = 0
-    for day_sales in day_sales_counts do
-      if day_sales.sales != sales
+    for sales_count in sales_counts do
+      if sales_count['sales'].to_i != sales
         rank += 1
-        sales = day_sales.sales
+        sales = sales_count['sales'].to_i
       end
       ranking = SalesPerformanceRank.find_or_initialize_by day: day,
-                                                           rankable: day_sales.saleable
-      ranking.day_rank = rank
-      ranking.save
-    end
-  end
-
-  def self.process_week_rankings(week_sales_counts, day, type)
-    rank = 0
-    sales = 0
-    for week_sales in week_sales_counts do
-      if week_sales['sales'].to_i != sales
-        rank += 1
-        sales = week_sales['sales'].to_i
-      end
-      ranking = SalesPerformanceRank.find_or_initialize_by day: day,
-                                                           rankable_id: week_sales['id'].to_i,
+                                                           rankable_id: sales_count['id'].to_i,
                                                            rankable_type: type
-      ranking.week_rank = rank
+      ranking[column] = rank
       ranking.save
     end
   end
-
-  def self.process_month_rankings(month_sales_counts, day, type)
-    rank = 0
-    sales = 0
-    for month_sales in month_sales_counts do
-      if month_sales['sales'].to_i != sales
-        rank += 1
-        sales = month_sales['sales'].to_i
-      end
-      ranking = SalesPerformanceRank.find_or_initialize_by day: day,
-                                                           rankable_id: month_sales['id'].to_i,
-                                                           rankable_type: type
-      ranking.month_rank = rank
-      ranking.save
-    end
-  end
-
 end
