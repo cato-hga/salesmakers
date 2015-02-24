92480ee9263d0ec2897c68380e911ea47ac3544d
stephen <milessa42@gmail.com>
2015-02-24 16:31:21 -0500

    Swaps with two lines view completed...now to work on the actual units...

--- spec/features/lines/line_swapping_spec.rb
+++ spec/features/lines/line_swapping_spec.rb
@@ -10,8 +10,9 @@ describe 'Swapping lines' do
   let(:permission_index) { Permission.new key: 'device_index',
                                           permission_group: permission_group,
                                           description: 'Test Description' }
-  let!(:device) { create :device }
+
   describe 'for unauthorized users' do
+    let!(:device) { create :device }
     let(:unauth_person) { create :person }
     before(:each) do
       CASClient::Frameworks::Rails::Filter.fake(unauth_person.email)
@@ -24,6 +25,7 @@ describe 'Swapping lines' do
   end
 
   describe 'for authorized users' do
+    let!(:device) { create :device }
     let(:line_state) { LineState.find_or_initialize_by name: 'Active' }
     let!(:line) { create :line, identifier: '7274985180', line_states: [line_state] }
     let!(:second_line) { create :line, identifier: '7274985181', line_states: [line_state] }
@@ -110,11 +112,11 @@ describe 'Swapping lines' do
   end
 
   describe 'when both lines have devices' do
-    let(:device_one) { create :device, line: line, serial: '215899', identifier: '215899' }
+    let!(:device_one) { create :device, line: line, serial: '215899', identifier: '215899' }
     let(:line_state) { LineState.find_or_initialize_by name: 'Active' }
     let!(:line) { create :line, identifier: '7274985180', line_states: [line_state] }
     let!(:second_line) { create :line, identifier: '7274985181', line_states: [line_state] }
-    let(:device_two) { create :device, line: second_line }
+    let!(:device_two) { create :device, serial: '998512', identifier: '998512', line: second_line }
 
     before(:each) do
       CASClient::Frameworks::Rails::Filter.fake(person.email)
@@ -125,37 +127,41 @@ describe 'Swapping lines' do
     end
 
     describe 'the swap results page' do
-      it 'prompts the user to pick between deactivated the swapped line or keeping it active' do
+      it 'DOES NOT prompt the user to pick between deactivated the swapped line or keeping it active' do
         expect(page).to have_content('Swap Results')
-        expect(page).to have_content("Do you want to deactivate the device's original line or keep it active?")
-      end
-      it 'shows deactivation information', js: true do
-        Capybara.ignore_hidden_elements = false
-        click_on "Deactivate #{line.identifier}"
-        expect(page).to have_css('div#swap_results_deactivate.widget.visible')
-        expect(page).to have_css('div#swap_results_keep_active.widget.hidden')
-        expect(page).to have_content('7274985180 will be removed from 215899 and marked as inactive')
-        expect(page).to have_content('7274985181 will be moved to 215899')
-        expect(page).to have_content('Confirm Swap')
-        Capybara.ignore_hidden_elements = true
+        expect(page).not_to have_content("Do you want to deactivate the device's original line or keep it active?")
       end
 
-      it 'shows keeping active information', js: true do
-        Capybara.ignore_hidden_elements = false
-        click_on "Keep #{line.identifier} Active"
-        expect(page).to have_css('div#swap_results_deactivate.widget.hidden')
-        expect(page).to have_css('div#swap_results_keep_active.widget.visible')
-        expect(page).to have_content('7274985180 will be removed from 215899 and NOT marked as inactive')
-        expect(page).to have_content('7274985181 will be moved to 215899')
+      it 'shows the swap' do
+        expect(page).to have_content('7274985180 will be removed from 215899 and replaced by 7274985181')
+        expect(page).to have_content('7274985181 will be removed from 998512 and replaced by 7274985180')
         expect(page).to have_content('Confirm Swap')
-        Capybara.ignore_hidden_elements = true
       end
     end
 
     describe 'submission success' do
-      it 'shows a confirmation'
-      it 'redirects to the original device'
-      it 'creates log entries'
+      before(:each) do
+        within('#swap_results_two_lines') do
+          click_on('Confirm Swap')
+        end
+      end
+      it 'shows a confirmation' do
+        expect(page).to have_content 'Line(s) swapped!'
+      end
+      it 'redirects to the original device' do
+        within('header h1') do
+          expect(page).to have_content device_one.serial
+        end
+      end
+      it 'creates log entries' do
+        within('.history') do
+          expect(page).to have_content "Swapped the device's line from #{line.identifier}"
+        end
+        visit device_path device_two
+        within('.history') do
+          expect(page).to have_content "Swapped the device's line from #{second_line.identifier}"
+        end
+      end
     end
 
     describe 'submission failure' do
