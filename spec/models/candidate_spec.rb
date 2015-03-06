ef2f5870db54c699ac960f22728cb59b07eb683e
stephen <milessa42@gmail.com>
2015-03-06 14:39:50 -0500

    tests for zip codes

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -20,8 +20,12 @@ describe Candidate do
       candidate.email = nil
       expect(candidate).not_to be_valid
     end
-    it 'requires a zip code' do
+    it 'requires a zip code exactly 5 digits long' do
       candidate.zip = nil
+      expect(candidate).not_to be_valid
+      candidate.zip = '1234'
+      expect(candidate).not_to be_valid
+      candidate.zip = '123456'
       expect(candidate).not_to be_valid
     end
     it 'requires a project' do
