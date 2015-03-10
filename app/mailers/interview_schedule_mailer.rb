9875e4b00e52e8cad500a43f766a6428dd597e7f
stephen <milessa42@gmail.com>
2015-03-10 10:28:51 -0400

    Troubleshoot some more

--- app/mailers/interview_schedule_mailer.rb
+++ app/mailers/interview_schedule_mailer.rb
@@ -1,11 +1,11 @@
 class InterviewScheduleMailer < ApplicationMailer
   default from: 'interviewscheduling@salesmakersinc.com'
 
-  def interview_mailer(candidate, recruiter, schedule, cloud_room)
+  def interview_mailer(candidate, recruiter, schedule)
     @candidate = candidate
     @recruiter = recruiter
     @schedule = schedule
-    @cloud_room = cloud_room
+    #@cloud_room = cloud_room
     mail(to: candidate.email, cc: recruiter.email, subject: 'Your interview with SalesMakers, Inc')
   end
 end
