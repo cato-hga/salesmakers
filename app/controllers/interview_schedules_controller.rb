71e507a63a37a65795f3d88489e8946a056ee3e4
stephen <milessa42@gmail.com>
2015-03-13 10:56:59 -0400

    Refactored interview schedules controller --skip-ci

--- app/controllers/interview_schedules_controller.rb
+++ app/controllers/interview_schedules_controller.rb
@@ -24,28 +24,13 @@ class InterviewSchedulesController < ApplicationController
   def create
     @cloud_room = params[:cloud_room]
     @interview_schedule = InterviewSchedule.new
-    interview_date = params[:interview_date].to_date
-    @interview_schedule.interview_date = interview_date
-    hour = params[:interview_time].slice 0..1
-    minutes = params[:interview_time].slice 2..3
-    interview_time = Time.zone.local(interview_date.year, interview_date.month, interview_date.day, hour, minutes)
-    @interview_schedule.start_time = interview_time
-    @interview_schedule.person = @current_person
-    @interview_schedule.candidate = @candidate
+    get_date_and_time
+    assign_schedule_attributes
     if @interview_schedule.save
-      @candidate.interview_scheduled!
-      InterviewScheduleMailer.interview_mailer(@candidate, @current_person, @interview_schedule, @cloud_room).deliver_later
-      @current_person.log? 'scheduled_for_interview',
-                           @candidate,
-                           @interview_schedule
-      flash[:notice] = 'Candidate scheduled!'
-      redirect_to @candidate
+      schedule_candidate
     else
-      #TODO: HELP
       puts @interview_schedule.errors.full_messages.join(', ')
     end
-
-
   end
 
   def schedule
@@ -65,7 +50,6 @@ class InterviewSchedulesController < ApplicationController
                            @interview_schedule
       redirect_to new_candidate_interview_answer_path @candidate
     else
-      #TODO: HELP
       puts @interview_schedule.errors.full_messages.join(', ')
     end
   end
@@ -76,30 +60,66 @@ class InterviewSchedulesController < ApplicationController
       flash[:error] = 'Cloud room is required'
       redirect_to new_candidate_interview_schedule_path @candidate and return
     end
-    interview_chronic = Chronic.parse params[:interview_date]
-    @interview_date = interview_chronic.to_date if interview_chronic
-    if @interview_date == nil
-      flash[:error] = 'The date entered could not be used - there may be a typo or invalid date. Please re-enter'
-      redirect_to new_candidate_interview_schedule_path @candidate and return
-    end
+    get_and_handle_inputted_date
+    create_taken_time_slots
+    create_available_time_slots
+  end
+
+  private
+
+  def create_taken_time_slots
     scheduled_interviews = InterviewSchedule.where(interview_date: @interview_date,
                                                    person: @current_person)
-    taken_time_slots = []
+    @taken_time_slots = []
     for interview in scheduled_interviews do
-      taken_time_slots << interview.start_time.in_time_zone.strftime('%H%M')
+      @taken_time_slots << interview.start_time.in_time_zone.strftime('%H%M')
     end
+  end
+
+  def create_available_time_slots
     time_slots_start = Time.zone.local(@interview_date.year, @interview_date.month, @interview_date.day, 9, 0, 0)
     @time_slots = []
     24.times do
       time_slot = time_slots_start.strftime('%H%M')
-      unless taken_time_slots.include? time_slot
+      unless @taken_time_slots.include? time_slot
         @time_slots << time_slots_start
       end
       time_slots_start = time_slots_start + 30.minutes
     end
   end
 
-  private
+  def get_and_handle_inputted_date
+    interview_chronic = Chronic.parse params[:interview_date]
+    @interview_date = interview_chronic.to_date if interview_chronic
+    if @interview_date == nil
+      flash[:error] = 'The date entered could not be used - there may be a typo or invalid date. Please re-enter'
+      redirect_to new_candidate_interview_schedule_path @candidate and return
+    end
+  end
+
+  def assign_schedule_attributes
+    @interview_schedule.start_time = @interview_time
+    @interview_schedule.person = @current_person
+    @interview_schedule.candidate = @candidate
+  end
+
+  def schedule_candidate
+    @candidate.interview_scheduled!
+    InterviewScheduleMailer.interview_mailer(@candidate, @current_person, @interview_schedule, @cloud_room).deliver_later
+    @current_person.log? 'scheduled_for_interview',
+                         @candidate,
+                         @interview_schedule
+    flash[:notice] = 'Candidate scheduled!'
+    redirect_to @candidate
+  end
+
+  def get_date_and_time
+    interview_date = params[:interview_date].to_date
+    @interview_schedule.interview_date = interview_date
+    hour = params[:interview_time].slice 0..1
+    minutes = params[:interview_time].slice 2..3
+    @interview_time = Time.zone.local(interview_date.year, interview_date.month, interview_date.day, hour, minutes)
+  end
 
   def set_candidate
     @candidate = Candidate.find params[:candidate_id]
