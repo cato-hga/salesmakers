e49c8d8628096d37be08a2ce50a8fb18f6f5aa85
stephen <milessa42@gmail.com>
2015-03-17 13:50:44 -0400

    Training class created

--- spec/factories/training_class.rb
+++ spec/factories/training_class.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     association :training_class_type
     association :training_time_slot
     association :person
-    date DateTime.today
+    date DateTime.new(2015, Date.today.month, Date.today.day, 12, 0, 0)
   end
 end
