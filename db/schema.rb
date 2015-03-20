a340bda0e6326f8846bca39a204023eeff638249
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-03-20 14:40:21 -0400

    "Not enough technology" CandidateDismissalReason

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320171037) do
+ActiveRecord::Schema.define(version: 20150320183904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -808,7 +808,6 @@ ActiveRecord::Schema.define(version: 20150320171037) do
     t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.float "friday", default: 0.0, null: false
-    t.integer "location_area_id"
     t.float "monday", default: 0.0, null: false
     t.string "name", null: false
     t.float "saturday", default: 0.0, null: false
