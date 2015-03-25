6c4caa98bca94b7b22b2f3b192c569426e29c54d
stephen <milessa42@gmail.com>
2015-03-25 18:08:24 -0400

    Temp push to get staging radio shack locations geocoding

--- app/models/sprint_radio_shack_training_location.rb
+++ app/models/sprint_radio_shack_training_location.rb
@@ -1,9 +1,9 @@
 class SprintRadioShackTrainingLocation < ActiveRecord::Base
   geocoded_by :address
-  after_validation :geocode_on_production,
-                   if: ->(location) {
-                     location.address and location.address_changed? and not location.virtual?
-                   }
+  after_validation :geocode_on_production #,
+  # if: ->(location) {
+  #   location.address and location.address_changed? and not location.virtual?
+  # }
   validates :name, presence: true
   validates :room, presence: true
   validates :virtual, inclusion: {in: [true, false]}
@@ -15,7 +15,8 @@ class SprintRadioShackTrainingLocation < ActiveRecord::Base
   private
 
   def geocode_on_production
-    return unless Rails.env.production?
+    #return unless Rails.env.production?
     self.geocode
+    sleep 0.21
   end
 end
