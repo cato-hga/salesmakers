f30840f35b334ea4be0c9b0482dba7d06f0e9109
stephen <milessa42@gmail.com>
2015-03-25 18:46:09 -0400

    Restored geocode protection for radio shack training locations

--- app/models/sprint_radio_shack_training_location.rb
+++ app/models/sprint_radio_shack_training_location.rb
@@ -1,9 +1,9 @@
 class SprintRadioShackTrainingLocation < ActiveRecord::Base
   geocoded_by :address
-  after_validation :geocode_on_production #,
-  # if: ->(location) {
-  #   location.address and location.address_changed? and not location.virtual?
-  # }
+  after_validation :geocode_on_production,
+                   if: ->(location) {
+                     location.address and location.address_changed? and not location.virtual?
+                   }
   validates :name, presence: true
   validates :room, presence: true
   validates :virtual, inclusion: {in: [true, false]}
