c710291d594e9c8742dc402db93fdc60d5a4c7f4
stephen <milessa42@gmail.com>
2015-03-25 09:14:14 -0400

    Changed codeship capistrano actions, now should pass the branch to the staging.rb file for cap

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,7 @@ set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :branch, fetch(:branch)
 
 role :app, %w{deploy@staging.salesmakersinc.com}
 role :web, %w{deploy@staging.salesmakersinc.com}
