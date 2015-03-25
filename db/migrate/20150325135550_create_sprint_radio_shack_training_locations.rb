6af8bf7a2c25fe50ee8d2fc973fb8eb01ac95875
stephen <milessa42@gmail.com>
2015-03-25 11:55:13 -0400

    Dropping and re-adding table

--- db/migrate/20150325135550_create_sprint_radio_shack_training_locations.rb
+++ db/migrate/20150325135550_create_sprint_radio_shack_training_locations.rb
@@ -1,14 +1,5 @@
 class CreateSprintRadioShackTrainingLocations < ActiveRecord::Migration
   def change
-    create_table :sprint_radio_shack_training_locations do |t|
-      t.string :name, null: false
-      t.string :address, null: false
-      t.string :room, null: false
-      t.float :latitude
-      t.float :longitude
-      t.boolean :virtual, null: false, default: false
 
-      t.timestamps null: false
-    end
   end
 end
