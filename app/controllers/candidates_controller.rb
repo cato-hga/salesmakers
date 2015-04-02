82863864f10ab78fb901eb79d2911c159db10aa5
stephen <milessa42@gmail.com>
2015-04-02 14:50:58 -0400

    split candidates#create up to avoid double renderer errors

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -161,35 +161,60 @@ class CandidatesController < ApplicationController
     @back_to_confirm = params[:back_to_confirm] == 'true' ? true : false
     @previous_location_area = @candidate.location_area
     unless @candidate.update(location_area: @location_area)
-      flash[:error] = @candidate.errors.full_messages.join(', ')
-      redirect_to select_location_candidate_path(@candidate, @back_to_confirm.to_s) and return
+      candidate_not_updated and return
     end
     if @previous_location_area
-      @previous_location_area.update potential_candidate_count: @previous_location_area.potential_candidate_count - 1
-      if Candidate.statuses[@candidate.status] >= Candidate.statuses['accepted']
-        @previous_location_area.update offer_extended_count: @previous_location_area.offer_extended_count - 1
-      end
+      candidate_has_previous_location_area
     end
     if @location_area.outsourced?
-      @candidate.accepted!
-      @location_area.update potential_candidate_count: @location_area.potential_candidate_count + 1
-      @location_area.update offer_extended_count: @location_area.offer_extended_count + 1
-      flash[:notice] = 'Location chosen successfully.'
-      redirect_to new_candidate_training_availability_path(@candidate) and return
+      candidate_location_outsourced and return
     end
     @candidate.location_selected! if @candidate.status == 'entered'
     if @back_to_confirm
-      flash[:notice] = 'Location chosen successfully.'
-      redirect_to (new_candidate_training_availability_path(@candidate))
+      back_to_confirmation and return
     else
-      CandidatePrescreenAssessmentMailer.assessment_mailer(@candidate, @location_area.area).deliver_later
-      @current_person.log? 'sent_assessment',
-                           @candidate
-      if @candidate.prescreened?
-        @location_area.update potential_candidate_count: @location_area.potential_candidate_count + 1
-        flash[:notice] = 'Location chosen successfully. You were redirected to the candidate page because the candidate was already prescreened'
-        redirect_to candidate_path(@candidate) and return
-      end
+      candidate_location_completion
+    end
+  end
+
+  def candidate_not_updated
+    flash[:error] = @candidate.errors.full_messages.join(', ')
+    redirect_to select_location_candidate_path(@candidate, @back_to_confirm.to_s)
+  end
+
+  def candidate_has_previous_location_area
+    @previous_location_area.update potential_candidate_count: @previous_location_area.potential_candidate_count - 1
+    if Candidate.statuses[@candidate.status] >= Candidate.statuses['accepted']
+      @previous_location_area.update offer_extended_count: @previous_location_area.offer_extended_count - 1
+    end
+  end
+
+  def candidate_location_outsourced
+    @candidate.accepted!
+    @location_area.update potential_candidate_count: @location_area.potential_candidate_count + 1
+    @location_area.update offer_extended_count: @location_area.offer_extended_count + 1
+    flash[:notice] = 'Location chosen successfully.'
+    redirect_to new_candidate_training_availability_path(@candidate)
+  end
+
+  def candidate_is_prescreened
+    @location_area.update potential_candidate_count: @location_area.potential_candidate_count + 1
+    flash[:notice] = 'Location chosen successfully. You were redirected to the candidate page because the candidate was already prescreened'
+    redirect_to candidate_path(@candidate)
+  end
+
+  def back_to_confirm
+    flash[:notice] = 'Location chosen successfully.'
+    redirect_to (new_candidate_training_availability_path(@candidate))
+  end
+
+  def candidate_location_completion
+    CandidatePrescreenAssessmentMailer.assessment_mailer(@candidate, @location_area.area).deliver_later
+    @current_person.log? 'sent_assessment',
+                         @candidate
+    if @candidate.prescreened?
+      candidate_is_prescreened and return
+    else
       flash[:notice] = 'Location chosen successfully.'
       redirect_to new_candidate_prescreen_answer_path(@candidate)
     end
