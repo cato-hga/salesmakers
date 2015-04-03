d02cf50a3c49f7d99c59b7a14c2ae3a26ca60539
stephen <milessa42@gmail.com>
2015-04-03 14:59:59 -0400

    Moved availability params into a concern

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,6 +2,7 @@ require 'apis/gateway'
 
 class CandidatesController < ApplicationController
   include CandidateDashboard
+  include AvailabilityParams
 
   after_action :verify_authorized
   before_action :do_authorization
@@ -337,33 +338,6 @@ class CandidatesController < ApplicationController
 
   def sprint_radio_shack_training_session_params
     params.require(:sprint_radio_shack_training_session).permit :id
-  end
-
-  def availability_params
-    params.require(:candidate_availability).permit(
-        :monday_first,
-        :monday_second,
-        :monday_third,
-        :tuesday_first,
-        :tuesday_second,
-        :tuesday_third,
-        :wednesday_first,
-        :wednesday_second,
-        :wednesday_third,
-        :thursday_first,
-        :thursday_second,
-        :thursday_third,
-        :friday_first,
-        :friday_second,
-        :friday_third,
-        :saturday_first,
-        :saturday_second,
-        :saturday_third,
-        :sunday_first,
-        :sunday_second,
-        :sunday_third,
-        :comment
-    )
   end
 
   def search_bar
--- app/controllers/prescreen_answers_controller.rb
+++ app/controllers/prescreen_answers_controller.rb
@@ -1,4 +1,6 @@
 class PrescreenAnswersController < ApplicationController
+  include AvailabilityParams
+
   after_action :verify_authorized
   before_action :do_authorization
 
@@ -61,33 +63,6 @@ class PrescreenAnswersController < ApplicationController
                                              :worked_for_sprint,
                                              :eligible_smart_phone,
                                              :ok_to_screen
-    )
-  end
-
-  def availability_params
-    params.require(:candidate_availability).permit(
-        :monday_first,
-        :monday_second,
-        :monday_third,
-        :tuesday_first,
-        :tuesday_second,
-        :tuesday_third,
-        :wednesday_first,
-        :wednesday_second,
-        :wednesday_third,
-        :thursday_first,
-        :thursday_second,
-        :thursday_third,
-        :friday_first,
-        :friday_second,
-        :friday_third,
-        :saturday_first,
-        :saturday_second,
-        :saturday_third,
-        :sunday_first,
-        :sunday_second,
-        :sunday_third,
-        :comment
     )
   end
 
