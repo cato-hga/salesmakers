855150913153a004d443d50b0a3cb23f48a03ea2
stephen <milessa42@gmail.com>
2015-04-06 11:58:00 -0400

    Fix for screenings not being saved correctly

--- spec/controllers/screenings_controller_spec.rb
+++ spec/controllers/screenings_controller_spec.rb
@@ -23,25 +23,53 @@ describe ScreeningsController do
   end
 
   describe 'PUT update' do
-    before do
-      allow(controller).to receive(:policy).and_return double(update?: true)
-      put :update,
-          person_id: person.id,
-          screening: {
-              sex_offender_check: '2',
-              public_background_check: '1',
-              private_background_check: '3',
-              drug_screening: '3'
-          }
+    context 'for a new screen' do
+      before do
+        allow(controller).to receive(:policy).and_return double(update?: true)
+        put :update,
+            person_id: person.id,
+            screening: {
+                sex_offender_check: '2',
+                public_background_check: '1',
+                private_background_check: '3',
+                drug_screening: '3'
+            }
+      end
+
+      it 'creates a log entry for the person and candidate' do
+        expect(LogEntry.where(trackable_type: 'Candidate').count).to eq(1)
+        expect(LogEntry.where(trackable_type: 'Person').count).to eq(1)
+      end
+
+      it 'redirects to people#index' do
+        expect(response).to redirect_to(people_path)
+      end
     end
 
-    it 'creates a log entry for the person and candidate' do
-      expect(LogEntry.where(trackable_type: 'Candidate').count).to eq(1)
-      expect(LogEntry.where(trackable_type: 'Person').count).to eq(1)
+    context 'for an updated screening' do
+      let!(:screen) { create :screening, person: person, sex_offender_check: 1, public_background_check: 1, private_background_check: 1, drug_screening: 1 }
+      it 'updates the persons screening information' do
+        expect(person.screening.sex_offender_check).to eq('sex_offender_check_failed')
+        expect(person.screening.public_background_check).to eq('public_background_check_failed')
+        expect(person.screening.private_background_check).to eq('private_background_check_initiated')
+        expect(person.screening.drug_screening).to eq('drug_screening_sent')
+        allow(controller).to receive(:policy).and_return double(update?: true)
+        put :update,
+            person_id: person.id,
+            screening: {
+                sex_offender_check: '2',
+                public_background_check: '2',
+                private_background_check: '3',
+                drug_screening: '3'
+            }
+        person.reload
+        person.screening.reload
+        expect(person.screening.sex_offender_check).to eq('sex_offender_check_passed')
+        expect(person.screening.public_background_check).to eq('public_background_check_passed')
+        expect(person.screening.private_background_check).to eq('private_background_check_passed')
+        expect(person.screening.drug_screening).to eq('drug_screening_passed')
+      end
     end
 
-    it 'redirects to people#index' do
-      expect(response).to redirect_to(people_path)
-    end
   end
 end
