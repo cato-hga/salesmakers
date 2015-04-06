7cc63a52484e5929f6640a675154885176e78e86
stephen <milessa42@gmail.com>
2015-04-06 16:03:45 -0400

    fixing === vs == that codeclimate is complaining about.

--- app/assets/javascripts/prescreen_answers.js
+++ app/assets/javascripts/prescreen_answers.js
@@ -4,84 +4,84 @@
 $(function () {
     $(document).foundation();
     $('#monday_all').click(function () {
-        if ($(this).prop("checked") == true) {
+        if ($(this).prop("checked") === true) {
             $('#candidate_availability_monday_first').prop('checked', true);
             $('#candidate_availability_monday_second').prop('checked', true);
             $('#candidate_availability_monday_third').prop('checked', true);
         }
-        else if ($(this).prop("checked") == false) {
+        else if ($(this).prop("checked") === false) {
             $('#candidate_availability_monday_first').prop('checked', false);
             $('#candidate_availability_monday_second').prop('checked', false);
             $('#candidate_availability_monday_third').prop('checked', false);
         }
     });
     $('#tuesday_all').click(function () {
-        if ($(this).prop("checked") == true) {
+        if ($(this).prop("checked") === true) {
             $('#candidate_availability_tuesday_first').prop('checked', true);
             $('#candidate_availability_tuesday_second').prop('checked', true);
             $('#candidate_availability_tuesday_third').prop('checked', true);
         }
-        else if ($(this).prop("checked") == false) {
+        else if ($(this).prop("checked") === false) {
             $('#candidate_availability_tuesday_first').prop('checked', false);
             $('#candidate_availability_tuesday_second').prop('checked', false);
             $('#candidate_availability_tuesday_third').prop('checked', false);
         }
     });
     $('#wednesday_all').click(function () {
-        if ($(this).prop("checked") == true) {
+        if ($(this).prop("checked") === true) {
             $('#candidate_availability_wednesday_first').prop('checked', true);
             $('#candidate_availability_wednesday_second').prop('checked', true);
             $('#candidate_availability_wednesday_third').prop('checked', true);
         }
-        else if ($(this).prop("checked") == false) {
+        else if ($(this).prop("checked") === false) {
             $('#candidate_availability_wednesday_first').prop('checked', false);
             $('#candidate_availability_wednesday_second').prop('checked', false);
             $('#candidate_availability_wednesday_third').prop('checked', false);
         }
     });
     $('#thursday_all').click(function () {
-        if ($(this).prop("checked") == true) {
+        if ($(this).prop("checked") === true) {
             $('#candidate_availability_thursday_first').prop('checked', true);
             $('#candidate_availability_thursday_second').prop('checked', true);
             $('#candidate_availability_thursday_third').prop('checked', true);
         }
-        else if ($(this).prop("checked") == false) {
+        else if ($(this).prop("checked") === false) {
             $('#candidate_availability_thursday_first').prop('checked', false);
             $('#candidate_availability_thursday_second').prop('checked', false);
             $('#candidate_availability_thursday_third').prop('checked', false);
         }
     });
     $('#friday_all').click(function () {
-        if ($(this).prop("checked") == true) {
+        if ($(this).prop("checked") === true) {
             $('#candidate_availability_friday_first').prop('checked', true);
             $('#candidate_availability_friday_second').prop('checked', true);
             $('#candidate_availability_friday_third').prop('checked', true);
         }
-        else if ($(this).prop("checked") == false) {
+        else if ($(this).prop("checked") === false) {
             $('#candidate_availability_friday_first').prop('checked', false);
             $('#candidate_availability_friday_second').prop('checked', false);
             $('#candidate_availability_friday_third').prop('checked', false);
         }
     });
     $('#saturday_all').click(function () {
-        if ($(this).prop("checked") == true) {
+        if ($(this).prop("checked") === true) {
             $('#candidate_availability_saturday_first').prop('checked', true);
             $('#candidate_availability_saturday_second').prop('checked', true);
             $('#candidate_availability_saturday_third').prop('checked', true);
         }
-        else if ($(this).prop("checked") == false) {
+        else if ($(this).prop("checked") === false) {
             $('#candidate_availability_saturday_first').prop('checked', false);
             $('#candidate_availability_saturday_second').prop('checked', false);
             $('#candidate_availability_saturday_third').prop('checked', false);
         }
     });
     $('#sunday_all').click(function () {
-        if ($(this).prop("checked") == true) {
+        if ($(this).prop("checked") === true) {
             $('#candidate_availability_sunday_first').prop('checked', true);
             $('#candidate_availability_sunday_second').prop('checked', true);
             $('#candidate_availability_sunday_third').prop('checked', true);
         }
-        else if ($(this).prop("checked") == false) {
+        else if ($(this).prop("checked") === false) {
             $('#candidate_availability_sunday_first').prop('checked', false);
             $('#candidate_availability_sunday_second').prop('checked', false);
             $('#candidate_availability_sunday_third').prop('checked', false);
