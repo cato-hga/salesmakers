8a1247981b8213ce04a0e68b2a3b83d23d1f2a88
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-04-10 16:40:07 -0400

    CodeClimate complexity cleanup.

--- app/policies/group_me_group_policy.rb
+++ app/policies/group_me_group_policy.rb
@@ -1,16 +1,25 @@
 class GroupMeGroupPolicy < ApplicationPolicy
   class Scope < Struct.new(:person, :scope)
     def resolve
-      return scope.none unless self.person and self.person.position
-      if self.person.position.hq? and self.person.position.all_field_visibility?
-        return scope.all
-      end
+      return scope.none unless has_positon?
+      return scope.all if has_all_visibility?
       management_person_areas = self.person.person_areas.where(manages: true)
       return scope.none if management_person_areas.empty?
       areas = management_person_areas.
           map{ |pa| pa.area.subtree.ids }.
           flatten
       scope.where('group_me_groups.area_id IN (?)', areas.flatten.uniq)
+    end
+
+    private
+
+    def has_position?
+      self.person and self.person.position
+    end
+
+    def has_all_visibility?
+      self.person.position.hq? and
+          self.person.position.all_field_visibility?
     end
   end
 
