88f5960d2e8ec0c078ffe01d0bdf48e9e25fdb38
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-04-13 22:16:26 -0400

    ComcastSale scopes cleanup.

---
+++ app/models/comcast/sales_and_leads.rb
@@ -0,0 +1,24 @@
+module Comcast::SalesAndLeads
+
+  private
+
+  def comcast_customer_name
+    self.comcast_customer.name
+  end
+
+  def comcast_customer_mobile_phone
+    self.comcast_customer.mobile_phone
+  end
+
+  def comcast_customer_other_phone
+    self.comcast_customer.other_phone
+  end
+
+  def one_service_selected
+    unless self.tv? or self.internet? or self.phone? or self.security?
+      [:tv, :internet, :phone, :security].each do |product|
+        errors.add(product, 'or at least one other product must be selected')
+      end
+    end
+  end
+end
--- app/models/comcast_sale.rb
+++ app/models/comcast_sale.rb
@@ -1,4 +1,10 @@
+require 'comcast/sales_and_leads'
+require 'comcast_sale_scopes'
+
 class ComcastSale < ActiveRecord::Base
+  include Comcast::SalesAndLeads
+  extend ComcastSaleScopes
+
   validates :order_date, presence: true
   validates :person_id, presence: true
   validates :comcast_customer_id, presence: true
@@ -15,49 +21,7 @@ class ComcastSale < ActiveRecord::Base
   has_one :comcast_former_provider
   has_one :comcast_lead
 
-  default_scope {
-    joins(:person, :comcast_install_appointment).
-        order('comcast_install_appointments.install_date DESC, people.display_name ASC')
-  }
-
-  scope :person, ->(person_id) {
-    where(person_id: person_id)
-  }
-
-  scope :recent_installations, -> {
-    joins(:comcast_install_appointment).
-        where('comcast_install_appointments.install_date > ?',
-              Date.today - 1.week).
-        where('comcast_install_appointments.install_date < ?',
-              Date.today).
-        order('comcast_install_appointments.install_date')
-  }
-
-  scope :upcoming_installations, -> {
-    joins(:comcast_install_appointment).
-        where('comcast_install_appointments.install_date < ?',
-              Date.today + 1.week).
-        where('comcast_install_appointments.install_date >= ?',
-              Date.today).
-        order('comcast_install_appointments.install_date')
-  }
-
-  scope :sold_between_dates, ->(start_date, end_date) {
-    return none unless start_date and end_date
-    where('order_date >= ? AND order_date < ?', start_date, end_date)
-  }
-
-  def comcast_customer_name
-    self.comcast_customer.name
-  end
-
-  def comcast_customer_mobile_phone
-    self.comcast_customer.mobile_phone
-  end
-
-  def comcast_customer_other_phone
-    self.comcast_customer.other_phone
-  end
+  set_scopes
 
   private
 
@@ -65,14 +29,6 @@ class ComcastSale < ActiveRecord::Base
     return unless self.order_date
     if self.order_date.to_date < Date.today - 1.day
       errors.add(:order_date, 'cannot be more than 24 hours in the past')
-    end
-  end
-
-  def one_service_selected
-    unless self.tv? or self.internet? or self.phone? or self.security?
-      [:tv, :internet, :phone, :security].each do |product|
-        errors.add(product, 'or at least one other product must be selected')
-      end
     end
   end
 
@@ -84,3 +40,11 @@ class ComcastSale < ActiveRecord::Base
   end
 
 end
+
+
+
+
+
+
+
+
