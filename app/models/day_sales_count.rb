5c794483d8db012a63131949754a1cfad0278af5
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-04-14 09:09:48 -0400

    Code Climate complexity and similarity cleanup.

--- app/models/day_sales_count.rb
+++ app/models/day_sales_count.rb
@@ -7,39 +7,47 @@ class DaySalesCount < ActiveRecord::Base
 
   belongs_to :saleable, polymorphic: true
 
-  scope :today, -> { where day: Date.today }
-  scope :yesterday, -> { where day: Date.yesterday }
-  scope :this_week, -> {
-    where('day >= ? AND day <= ?',
-          Date.today.beginning_of_week,
-          Date.today)
-  }
-  scope :last_week, -> {
-    where('day >= ? AND day <= ?',
-          Date.today.beginning_of_week - 1.week,
-          Date.today.beginning_of_week - 1.day)
-  }
-  scope :this_month, -> {
-    where('day >= ? AND day <= ?',
-          Date.today.beginning_of_month,
-          Date.today)
-  }
-  scope :last_month, -> {
-    where('day >= ? AND day <= ?',
-          Date.today.beginning_of_month - 1.month,
-          Date.today.beginning_of_month - 1.day)
-  }
-  scope :this_year, -> {
-    where('day >= ? AND day <= ?',
-          Date.today.beginning_of_year,
-          Date.today)
-  }
-
   scope :for_range, ->(range) {
     where('day >= ? AND day <= ?',
           range.first,
           range.last)
   }
+
+  def self.day_scopes
+    scope :today, -> { where day: Date.today }
+    scope :yesterday, -> { where day: Date.yesterday }
+  end
+
+  def self.week_scopes
+    scope :this_week, -> {
+      for_range Date.today.beginning_of_week..Date.today
+    }
+    scope :last_week, -> {
+      for_range (Date.today.beginning_of_week - 1.week)..
+                    (Date.today.beginning_of_week - 1.day)
+    }
+  end
+
+  def self.month_scopes
+    scope :this_month, -> {
+      for_range Date.today.beginning_of_month..Date.today
+    }
+    scope :last_month, -> {
+      for_range (Date.today.beginning_of_month - 1.month)..
+                    (Date.today.beginning_of_month - 1.day)
+    }
+  end
+
+  def self.year_scopes
+    scope :this_year, -> {
+      for_range Date.today.beginning_of_year..Date.today
+    }
+  end
+
+  day_scopes
+  week_scopes
+  month_scopes
+  year_scopes
 
   def self.import
     SaleImporter.new
