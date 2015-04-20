5209361b73f9899f0cfd3b6f0dbf39a5908d2ec8
stephen <milessa42@gmail.com>
2015-04-20 14:13:29 -0400

    Seeded permissions for HR and SSDs for termination

---
+++ db/migrate/20150420180755_add_terminate_permission_to_hq_positions.rb
@@ -0,0 +1,20 @@
+class AddTerminatePermissionToHqPositions < ActiveRecord::Migration
+  def change
+    group = PermissionGroup.find_or_create_by name: 'People'
+    terminate = Permission.create key: 'person_terminate',
+                                  permission_group: group,
+                                  description: 'can terminate people outside of their managed team members'
+
+
+    hr_dir = Position.find_by name: 'Human Resources Director'
+    hr_admin = Position.find_by name: 'Human Resources Administrator'
+    ssd = Position.find_by name: 'Senior Software Developer'
+
+    positions = [hr_dir, hr_admin, ssd]
+    for position in positions
+      position.permissions << terminate
+      position.save
+    end
+
+  end
+end
