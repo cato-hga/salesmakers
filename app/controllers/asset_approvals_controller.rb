362374ca596b3729dea4f9653742d9a70dff8e97
stephen <milessa42@gmail.com>
2015-04-24 16:03:33 -0400

    Refactor.

--- app/controllers/asset_approvals_controller.rb
+++ app/controllers/asset_approvals_controller.rb
@@ -2,42 +2,45 @@ class AssetApprovalsController < ApplicationController
 
   def approval
     authorize Person.new
-    team = @current_person.managed_team_members
-    people = []
-    for member in team do
-      project_name = member.person_areas.first.area.project.name
-      if project_name == 'Sprint Retail'
-        people << member if member.passed_asset_hours_requirement and member.sprint_prepaid_asset_approval_status == 'prepaid_no_decision'
-      end
-      if project_name.include? 'Vonage'
-        people << member if member.passed_asset_hours_requirement and member.vonage_tablet_approval_status == 'no_decision'
-      end
-    end
-    @pending_approval = Person.no_assets_from_collection(people)
+    @team = @current_person.managed_team_members
+    get_people
+    @pending_approval = Person.no_assets_from_collection(@people)
   end
 
   def approve
-    @person = Person.find params[:person_id]
-    project_name = @person.person_areas.first.area.project.name
-    if project_name.include? 'Sprint Retail'
-      @person.update sprint_prepaid_asset_approval_status: :prepaid_approved
-    else
-      @person.update vonage_tablet_approval_status: :approved
-    end
-    flash[:notice] = 'Employee approved for asset'
-    redirect_to asset_approval_path
+    set_status('approved')
   end
 
   def deny
+    set_status('denied')
+  end
+
+  private
+
+  def set_status(status)
     @person = Person.find params[:person_id]
     project_name = @person.person_areas.first.area.project.name
     if project_name.include? 'Sprint Retail'
-      @person.update sprint_prepaid_asset_approval_status: :prepaid_denied
+      @person.update sprint_prepaid_asset_approval_status: "prepaid_#{status}".to_sym
     else
-      @person.update vonage_tablet_approval_status: :denied
+      @person.update vonage_tablet_approval_status: "#{status}".to_sym
     end
-    flash[:notice] = 'Employee has been marked as "denied"'
+    flash[:notice] = "Employee has been marked as #{status} for asset"
     redirect_to asset_approval_path
+  end
+
+  def get_people
+    @people = []
+    for member in @team do
+      project_name = member.person_areas.first.area.project.name
+      if project_name == 'Sprint Retail'
+        @people << member if member.passed_asset_hours_requirement and member.sprint_prepaid_asset_approval_status == 'prepaid_no_decision'
+      end
+      if project_name.include? 'Vonage'
+        @people << member if member.passed_asset_hours_requirement and member.vonage_tablet_approval_status == 'no_decision'
+      end
+    end
+    @people
   end
 
 end
