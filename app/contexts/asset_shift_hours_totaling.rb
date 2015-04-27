a741b677294a1593730fe91fb39126ed3adb3122
stephen <milessa42@gmail.com>
2015-04-27 09:41:52 -0400

    Change to asset shift hours totalling

--- app/contexts/asset_shift_hours_totaling.rb
+++ app/contexts/asset_shift_hours_totaling.rb
@@ -2,6 +2,7 @@ class AssetShiftHoursTotaling
 
   def initialize(duration)
     @duration = duration
+    generate_totals
   end
 
   def generate_totals
@@ -13,7 +14,7 @@ class AssetShiftHoursTotaling
     end
     for person in people do
       person_hours = Shift.where(person: person).sum(:hours).to_i
-      if person_hours > 40 or person.project == prepaid
+      if person_hours > 40 or (person.project and person.project == prepaid)
         person.update passed_asset_hours_requirement: true
       end
     end
