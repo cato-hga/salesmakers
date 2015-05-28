1a34d5ab959d365b56b7f728c2d42d2f8a91db0f
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-05-28 08:45:08 -0400

    No map on the "Physical Address" section of People#show

--- app/assets/javascripts/people.js
+++ app/assets/javascripts/people.js
@@ -20,9 +20,6 @@
 //= require chartkick
 //= require masonry.min
 //= require underscore.min
-//= require google_maps_api.min
-//= require marker_clusterer.min
-//= require gmaps/google
 //= require sms_counter
 
 $(function(){
