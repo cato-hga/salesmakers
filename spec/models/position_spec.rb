c91d8a70b8b5e963bd08001b15ab79555b29cd71
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-06-25 14:23:38 -0400

    "Juan" position.

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -20,7 +20,7 @@ require 'shoulda/matchers'
 
 RSpec.describe Position, :type => :model do
 
-  it { should ensure_length_of(:name).is_at_least(5) }
+  it { should ensure_length_of(:name).is_at_least(4) }
   it { should validate_presence_of(:department) }
   it { should have_and_belong_to_many(:permissions) }
 
