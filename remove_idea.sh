d5461fef384bb4926a92edef491be7b425d509be
stephen <milessa42@gmail.com>
2015-07-01 11:08:06 -0400

    Removed .idea files from repo

---
+++ remove_idea.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git rm -r --cached .idea
+git rm -r --cached .idea/vcs.xml
+git rm -r --cached .idea/workspace.xml
+git rm -r --cached .idea/misc.xml
+git rm -r --cached .idea/reconnect.iml
