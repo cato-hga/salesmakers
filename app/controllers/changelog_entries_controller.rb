397dd27e232a3193feff7fc87bf1790f4535ee5a
salesmakersit <it@salesmakersinc.com>
2015-07-15 13:26:58 +0000

    Change default order of ChangelogEntries to be by released descending.

--- app/controllers/changelog_entries_controller.rb
+++ app/controllers/changelog_entries_controller.rb
@@ -4,7 +4,7 @@ class ChangelogEntriesController < ApplicationController
   before_action :chronic_time_zones
 
   def index
-    @changelog_entries = ChangelogEntry.order('Released').page(params[:page])
+    @changelog_entries = ChangelogEntry.order(released: :desc).page(params[:page])
   end
 
   def new
