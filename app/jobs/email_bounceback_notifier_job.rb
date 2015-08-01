e0f3058b3f05c2e29c975ad7fd558686ca710636
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-08-01 15:53:24 -0400

    Rescue Postmark::TimeoutError during bounces retrieval and retry a few times before giving up.

--- app/jobs/email_bounceback_notifier_job.rb
+++ app/jobs/email_bounceback_notifier_job.rb
@@ -4,19 +4,20 @@ class EmailBouncebackNotifierJob < ActiveJob::Base
   def perform minutes, automated = false
     after_datetime = DateTime.now - minutes.minutes
     client = Postmark::ApiClient.new Postmark.api_token
-    for bounce in bounces do
+    begin
       bounces = client.bounces.first(minutes * 4)
-      if bounce[:bounced_at] and bounce[:bounced_at].to_datetime >= after_datetime
-        attempts = 1
-        begin
+      for bounce in bounces do
+        if bounce[:bounced_at] and bounce[:bounced_at].to_datetime >= after_datetime
+          attempts = 1
           NotificationMailer.email_bounceback(bounce).deliver_now
-        rescue Postmark::TimeoutError => ex
-          Rails.logger.error "Error: #{ex}"
-          sleep 5
-          retry if (attempts += 1) <= 3
         end
       end
+    rescue Postmark::TimeoutError => ex
+      Rails.logger.error "Error: #{ex}"
+      sleep 5
+      retry if (attempts += 1) <= 3
     end
-    ProcessLog.create process_class: "EmailBouncebackNotifierJob", records_processed: bounces.count if automated
   end
+
+  ProcessLog.create process_class: "EmailBouncebackNotifierJob", records_processed: bounces.count if automated
 end
