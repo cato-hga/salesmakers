7bcbed2e756f8ffec4d381c0d6df7e1fe4b20c2f
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-08-02 15:13:55 -0400

    Whoops... It helps if you put the "end" keywords in the right place.

--- app/jobs/email_bounceback_notifier_job.rb
+++ app/jobs/email_bounceback_notifier_job.rb
@@ -17,7 +17,7 @@ class EmailBouncebackNotifierJob < ActiveJob::Base
       sleep 5
       retry if (attempts += 1) <= 3
     end
+    ProcessLog.create process_class: "EmailBouncebackNotifierJob", records_processed: bounces.count if automated
   end
 
-  ProcessLog.create process_class: "EmailBouncebackNotifierJob", records_processed: bounces.count if automated
 end
