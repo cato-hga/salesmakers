c7578d38bc888b945b8196237a23805e243e8053
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-08-04 16:13:26 -0400

    Geocoding with Bing Maps trial key.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,9 @@
 Geocoder.configure timeout: 25,
-                   lookup: :google_premier,
-                   #lookup: :nominatim,
-                   use_https: true,
-                   api_key: ['zE002EyHd5TAgBXB9cfdz57Gfio=', 'gme-salesmakersinc', 'SalesCenter_2']
+                   lookup: :bing,
+                   api_key: 'AgulBzKtAZ1zYM4HhaClc5EYxDXpOzjAth6gmGLlIrbsEoDPlbwt_i2n7YJ_oYlp'
+# Geocoder.configure timeout: 25,
+#                    lookup: :google_premier,
+#                    #lookup: :nominatim,
+#                    use_https: true,
+#                    api_key: ['zE002EyHd5TAgBXB9cfdz57Gfio=', 'gme-salesmakersinc', 'SalesCenter_2']
 #http_headers: { "User-Agent" => "SalesMakers, Inc. <developers@salesmakersinc.com>" }
