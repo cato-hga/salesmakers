d6aa7217a407b780a7dff5c31447ccc32d902383
stephen <milessa42@gmail.com>
2015-08-10 14:58:40 -0400

    All N+1 queries should be taken care of

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -115,7 +115,7 @@ class ApplicationController < BaseApplicationController
   end
 
   def set_current_user
-    @current_person = Person.find_by_email session[:cas_user] if session[:cas_user] #ME
+    @current_person ||= Person.find_by_email session[:cas_user] if session[:cas_user] #ME
     #@current_person = Person.find_by_email 'mvallejojr@retaildoneright.com'
     if not @current_person and not Rails.env.test?
       st = self.session[:cas_last_valid_ticket]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,34 +41,14 @@ Rails.application.configure do
 
   config.log_level = :debug
 
-  # config.after_initialize do
-  #   Bullet.enable = true
-  #   Bullet.alert = true
-  #   Bullet.bullet_logger = true
-  #   Bullet.console = true
-  #   Bullet.add_footer = true
-  # end
-
   # Needed for websocket-rails
   config.middleware.delete Rack::Lock
   #config.time_zone = 'Central Time (US & Canada)'
   config.after_initialize do
-    Bullet.enable = false
+    Bullet.enable = true
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
-    # Bullet.growl = true
-    # Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
-    #                 :password => 'bullets_password_for_jabber',
-    #                 :receiver => 'your_account@jabber.org',
-    #                 :show_online_status => true }
     Bullet.rails_logger = true
-    # Bullet.honeybadger = true
-    # Bullet.bugsnag = true
-    # Bullet.airbrake = true
-    # Bullet.rollbar = true
-    # Bullet.add_footer = true
-    # Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
-    # Bullet.slack = { webhook_url: 'http://some.slack.url', foo: 'bar' }
   end
 end
