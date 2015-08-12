e7f9d4092d6062679afa717aaff5d614bae75c27
John Cato <hga813@gmail.com>
2015-08-12 11:30:32 -0400

    removed files

--- spec/features/vonage_inventory/vonage_inventory_reclaiming_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-let(:person) { create :person, position: position}
-let(:position) {create :position, permissions: [permission_create] }
-let(:permission_group) { PermissionGroup.new name: 'Test Permission Group'}
-let(:permission_create) { Permission.new key: 'vonage_inventory_receive_create',
-                                         permission_group: permission_group,
-                                         description: 'Test Description' }
