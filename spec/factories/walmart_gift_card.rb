9a0997f6e2cf9f4459bc683b4500b95a1e9b56cd
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-08-21 08:46:18 -0400

    Challenge code on WalmartGiftCard factory.

--- spec/factories/walmart_gift_card.rb
+++ spec/factories/walmart_gift_card.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :walmart_gift_card do
     used false
     card_number '1234567890123456'
+    challenge_code 'hfg8uet'
     link 'https://egiftcard.walmart.com/blahblah'
     pin '4567'
     balance 60.00
