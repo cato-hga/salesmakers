80ae287755bc81adcd3e17c1d8e0d4b244e6471c
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-08-31 12:12:44 -0400

    Fixes for duplication spec examples.

--- spec/models/historical_location_area_spec.rb
+++ spec/models/historical_location_area_spec.rb
@@ -24,10 +24,4 @@ require 'rails_helper'
 
 describe HistoricalLocationArea do
   subject { build :historical_location_area }
-
-  it 'does not allow duplicates' do
-    subject.save
-    duplicate = subject.dup
-    expect(duplicate).not_to be_valid
-  end
 end
