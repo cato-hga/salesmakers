88de453f42c9666f022796789bbe521155fe4ce2
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-09-01 10:17:18 -0400

    Validate uniqueness of gift card numbers.

--- app/models/walmart_gift_card.rb
+++ app/models/walmart_gift_card.rb
@@ -25,7 +25,7 @@ class WalmartGiftCard < ActiveRecord::Base
   include WalmartGiftCards::Checks
 
   validates :used, inclusion: { in: [true, false] }
-  validates :card_number, length: { is: 16 }
+  validates :card_number, length: { is: 16 }, uniqueness: true
   validates :link, format: /\Ahttps?:\/\/.*\..*\....?\/.+\z/
   validates :pin, format: /\A[0-9]{4}\z/
   validates :balance, numericality: { greater_than_or_equal_to: 0.0 }
