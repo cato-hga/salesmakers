0defc1e376453f93a77ef44d5d37ac18eb07efb3
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-09-01 09:08:39 -0400

    Annotations, ConnectWalmartGiftCard and ConnectOverrideGiftCard.

---
+++ app/models/connect_override_gift_card.rb
@@ -0,0 +1,6 @@
+class ConnectWalmartGiftCard < RealConnectModel
+  # Openbravo table name
+  self.table_name = 'rvon_override_card'
+  # Openbravo table primary key column
+  self.primary_key = 'rvon_override_card_id'
+end
