753927ae4ce0e5eda20bfc367511ccae1577cf31
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-09-16 10:08:26 -0400

    Adding Virgin Mobile's "CUSTOM" SprintRatePlan.

--- db/structure.sql
+++ db/structure.sql
@@ -10904,3 +10904,5 @@ INSERT INTO schema_migrations (version) VALUES ('20150915144551');
 
 INSERT INTO schema_migrations (version) VALUES ('20150915154050');
 
+INSERT INTO schema_migrations (version) VALUES ('20150916140224');
+
