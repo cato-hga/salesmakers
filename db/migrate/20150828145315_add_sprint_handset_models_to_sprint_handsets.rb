692fc74e7640a96d267c69a03b1785164000f2a1
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-09-16 11:29:31 -0400

    Attempt to correct a migration preventing CodeShip from running completely.

--- db/migrate/20150828145315_add_sprint_handset_models_to_sprint_handsets.rb
+++ db/migrate/20150828145315_add_sprint_handset_models_to_sprint_handsets.rb
@@ -1,13 +1,15 @@
 class AddSprintHandsetModelsToSprintHandsets < ActiveRecord::Migration
   def up
-    boost = SprintCarrier.find_by name: 'Boost Mobile' || return
+    boost = SprintCarrier.find_by name: 'Boost Mobile'
+    return unless boost
     boost_models = ['Alcatel Onetouch Fling', 'Boost Warp 4G', 'HTC Desire 510', 'Kyocera Hydro', 'Kyocera Hydro Edge',
                     'Kyocera Icon', 'Kyocera Verve', 'LG Realm', 'LG Volt', 'Moto G', 'Nokia Lumia 635', 'Samsung Galaxy Prevail 2',
                     'Samsung Galaxy Rush', 'Sharp Aquos', 'ZTE Speed', 'ZTE Warp Sequent', 'Other Device'
     ]
     boost_models.each { |model_name| SprintHandset.create name: model_name, carrier_id: boost.id }
 
-    virgin = SprintCarrier.find_by name: 'Virgin Mobile' || return
+    virgin = SprintCarrier.find_by name: 'Virgin Mobile'
+    return unless virgin
     virgin_models = ['Custom', 'Kyocera Event', 'Kyocera Rise', 'Kyocera Vibe', 'LG Optimus F3', 'LG Volt', 'Samsung Galaxy Ring',
                      'ZTE Awe', 'ZTE Reef', 'Other Device'
     ]
@@ -17,15 +19,18 @@ class AddSprintHandsetModelsToSprintHandsets < ActiveRecord::Migration
     virgin_date_share_models = ['Galaxy Core Prime', 'HTC Desire 510', 'LG Tribute' 'LG Volt', 'Other Device']
     virgin_date_share_models.each { |model_name| SprintHandset.create name: model_name, carrier_id: virgin_mobile_date_share.id }
 
-    bb2go = SprintCarrier.find_by name: 'BB2Go' || return
+    bb2go = SprintCarrier.find_by name: 'BB2Go'
+    return unless bb2go
     bb2go_models = ['Mingle', 'Other Device']
     bb2go_models.each { |model_name| SprintHandset.create name: model_name, carrier_id: bb2go.id }
 
-    paylo = SprintCarrier.find_by name: 'payLo' || return
+    paylo = SprintCarrier.find_by name: 'payLo'
+    return unless paylo
     paylo_models = ['Kyocera Contact', 'Kyocera Kona', 'LG Aspire', 'Samsung Montage', 'Other Device']
     paylo_models.each { |model_name| SprintHandset.create name: model_name, carrier_id: paylo.id }
 
-    sprint = SprintCarrier.find_by name: 'Sprint' || return
+    sprint = SprintCarrier.find_by name: 'Sprint'
+    return unless sprint
     sprint_models = ['Apple iPhone 6 Plus', 'Apple iPhone 6', 'Apple iPhone 5S', 'Apple iPhone 5C', 'LG G3', 'Motorola Moto X',
                      'Samsung Galaxy Mega', 'Samsung Galaxy Note 4', 'Samsung Galaxy Note 3', 'Samsung Galaxy S5', 'Samsung Galaxy S4',
                      'Other Device']
