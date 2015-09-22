d9ab1c695ca161313c29d4cf20c0a6dd51f11059
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-09-22 09:55:42 -0400

    Fix for exception re: nil on bounces if getting a list of bounces from Postmark fails.

--- app/jobs/email_bounceback_notifier_job.rb
+++ app/jobs/email_bounceback_notifier_job.rb
@@ -20,8 +20,8 @@ class EmailBouncebackNotifierJob < ActiveJob::Base
         sleep 5
         retry if (attempts += 1) <= 3
       end
-      SlackJobNotifier.ping "[EmailBouncebackNotifierJob] Reported #{bounces.count.to_s} bounced email addresses." if bounces && bounces.count > 0
-      ProcessLog.create process_class: "EmailBouncebackNotifierJob", records_processed: bounces.count if automated
+      SlackJobNotifier.ping "[EmailBouncebackNotifierJob] Reported #{bounces.count.to_s} bounced email addresses." if bounces && bounces.count && bounces.count > 0
+      ProcessLog.create process_class: "EmailBouncebackNotifierJob", records_processed: bounces.count if automated && bounces && bounces.count
     ensure
       RunningProcess.shutdown! self
     end
