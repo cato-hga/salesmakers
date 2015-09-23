47291d8a37ea1ef9534fcef7d6fdee9a5de2aacf
Peter Ortiz <portiz@retaildoneright.com>
2015-09-23 09:10:51 -0400

    ran migrations

--- db/structure.sql
+++ db/structure.sql
@@ -11297,6 +11297,8 @@ INSERT INTO schema_migrations (version) VALUES ('20150918130721');
 
 INSERT INTO schema_migrations (version) VALUES ('20150918131000');
 
+INSERT INTO schema_migrations (version) VALUES ('20150918135649');
+
 INSERT INTO schema_migrations (version) VALUES ('20150922175218');
 
 INSERT INTO schema_migrations (version) VALUES ('20150922183804');
