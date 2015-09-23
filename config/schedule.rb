0acab466f4f86931db5095e9f81396778bfcb579
Anthony Atkinson <aatkinson@retaildoneright.com>
2015-09-23 11:50:08 -0400

    Destroy Ahoy::Events over 6 months old on a daily basis.

--- config/schedule.rb
+++ config/schedule.rb
@@ -28,7 +28,7 @@ set :path, '/opt/oneconnect/current'
 # 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45
 # 1  1  1  2     1  2  1  2  1  2  1  1  1
 # 46 47 48 49 50 51 52 53 54 55 56 57 58 59
-# 1  1  1  2     1  1  1  1  1  2  1  1  2
+# 1  1  1  2  1  1  1  1  1  1  2  1  1  2
 
 # ------------------------------------ MINUTES OF EVERY HOUR ----------------------------------------
 
@@ -66,6 +66,10 @@ end
 
 every '25,55 * * * *' do
   runner 'RingCentralCallLogger.log'
+end
+
+every '50 23 * * *' do
+  runner 'Ahoy::Event.where("time < ?", DateTime.now - 6.months).destroy_all'
 end
 
 every '10,40 * * * *' do
